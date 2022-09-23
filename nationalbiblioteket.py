1130f0d63586924fb648009520a83372ad1fb60a
xenial <farid.alijani@gmail.com>
2022-09-23 16:47:53 +0300

    lang no change just plot barh name in arg

--- nationalbiblioteket.py
+++ nationalbiblioteket.py
@@ -314,7 +314,7 @@ def main():
 	save_dfs(qlang=QUERY_LANGUAGE)
 
 	search_df, vol_df, pg_df = load_dfs( fpath=os.path.join(dpath, f"search_vol_pg_dfs_{QUERY_LANGUAGE}.dump") )
-	plt_bar(search_df, f"search_{QUERY_LANGUAGE})
+	plt_bar(search_df, name=f"search_{QUERY_LANGUAGE})
 
 
 
