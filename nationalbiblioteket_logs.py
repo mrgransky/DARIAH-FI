23b21ac734d1949af08c4db45c8628126a71c03f
xenial <farid.alijani@gmail.com>
2022-09-27 18:54:27 +0300

    log date clean with letters

--- nationalbiblioteket_logs.py
+++ nationalbiblioteket_logs.py
@@ -29,7 +29,7 @@ def get_df_no_ip_logs(infile=""):
 	with open(infile, mode="r") as f:
 		for line in f:
 			l = re.match(ACCESS_LOG_PATTERN, line).groups()
-			cleaned_lines.append({"time_received": 				l[0].replace("[|]", "").replace("/", "-"),
+			cleaned_lines.append({"time_received": 				l[0].replace("[", "").replace("]", "").replace("/", "-"),
 														"client_request_line": 	l[1],
 														"status": 							l[2],
 														"bytes_sent": 					l[3],
@@ -39,7 +39,6 @@ def get_df_no_ip_logs(infile=""):
 														})
 	
 	return pd.DataFrame.from_dict(cleaned_lines)
-
 
 if __name__ == '__main__':
 	os.system('clear')
