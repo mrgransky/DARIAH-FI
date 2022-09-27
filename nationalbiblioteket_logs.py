753c26ee717928abfe8f5e7038aab6266772e7bd
xenial <farid.alijani@gmail.com>
2022-09-27 19:29:16 +0300

    CHECK conv_dare

--- nationalbiblioteket_logs.py
+++ nationalbiblioteket_logs.py
@@ -1,5 +1,6 @@
 import os
 import re
+import datetime
 import numpy as np
 import pandas as pd
 
@@ -20,6 +21,30 @@ usr_ = {'alijani': '/lustre/sgn-data/vision',
 dpath = usr_[os.environ['USER']]
 
 
+def convert_date(INP_DATE):
+	months_dict = {
+		"Jan": "01", 
+		"Feb": "02", 
+		"Mar": "03", 
+		"Apr": "04", 
+		"May": "05", 
+		"Jun": "06", 
+		"Jul": "07", 
+		"Aug": "08", 
+		"Sep": "09", 
+		"Oct": "10", 
+		"Nov": "11", 
+		"Dec": "12", 
+		}
+
+	d_list = INP_DATE.split("-")
+	d_list[1] = months_dict.get(d_list[1])
+	MODIDFIED_DATE = '/'.join(d_list)
+	print(MODIDFIED_DATE)
+
+	yyyy_mm_dd = datetime.datetime.strptime(MODIDFIED_DATE, "%d/%m/%Y").strftime("%Y-%m-%d")
+	return yyyy_mm_dd
+
 def get_df_no_ip_logs(infile=""):
 	print(f">> Loading {infile} ...")
 	#ACCESS_LOG_PATTERN = '- - \[(.*?)\] "(.*?)" (\\d{3}) (.*) "([^\"]+)" "(.*?)" (\d+)'
@@ -29,20 +54,30 @@ def get_df_no_ip_logs(infile=""):
 	with open(infile, mode="r") as f:
 		for line in f:
 			l = re.match(ACCESS_LOG_PATTERN, line).groups()
-			cleaned_lines.append({"time_received": 				l[0].replace("[", "").replace("]", "").replace("/", "-"),
-														"client_request_line": 	l[1],
-														"status": 							l[2],
-														"bytes_sent": 					l[3],
-														"referer": 							l[4],
-														"user_agent": 					l[5],
-														"session_id": 					l[6], #TODO: must be check if key is a right name!
-														})
+
+			dt_tz = l[0].replace("[", "").replace("]", "")#.replace("/", "-")
+			
+			DDMMYYYY = dt_tz[:dt_tz.find(":")]
+			YYYYMMDD = convert_date( DDMMYYYY )
+			HMS = dt_tz[dt_tz.find(":")+1:dt_tz.find(" ")]
+			tz = dt_tz[dt_tz.find(" ")+1:]
+
+			cleaned_lines.append({
+				"date": 								l[0].replace("[", "").replace("]", "").replace("/", "-"),
+				"client_request_line": 	l[1],
+				"status": 							l[2],
+				"bytes_sent": 					l[3],
+				"referer": 							l[4],
+				"user_agent": 					l[5],
+				"session_id": 					l[6], #TODO: must be check if key is a right name!
+				})
 	
 	return pd.DataFrame.from_dict(cleaned_lines)
 
 if __name__ == '__main__':
 	os.system('clear')
 	fname = "nike6.docworks.lib.helsinki.fi_access_log.2021-02-07.log"
+	"""
 	df = get_df_no_ip_logs(infile=os.path.join(dpath, fname))
 	print(df.shape)
 	print("-"*130)
@@ -51,6 +86,8 @@ if __name__ == '__main__':
 	print("-"*130)
 
 	print( df.tail(40) )
+	"""
+	convert_date("07-Feb-2021")
 
 
 """
