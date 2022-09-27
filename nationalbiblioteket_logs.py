08f3445bdaeea429848966a5735c705c37d429a9
xenial <farid.alijani@gmail.com>
2022-09-27 19:38:07 +0300

    CHECK df print d_list check

--- nationalbiblioteket_logs.py
+++ nationalbiblioteket_logs.py
@@ -37,7 +37,7 @@ def convert_date(INP_DATE):
 		"Dec": "12", 
 		}
 
-	d_list = INP_DATE.split("-")
+	d_list = INP_DATE.split("/")
 	print(d_list)
 	d_list[1] = months_dict.get(d_list[1])
 	MODIDFIED_DATE = '/'.join(d_list)
