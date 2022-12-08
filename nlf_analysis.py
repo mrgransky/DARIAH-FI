008149a51c38b28556bf8e7e1de93fb03ca3260a
xenial <farid.alijani@gmail.com>
2022-12-08 13:12:58 +0200

    gen some results

--- nlf_analysis.py
+++ nlf_analysis.py
@@ -16,6 +16,7 @@ import matplotlib.pylab as pylab
 import seaborn as sns
 
 from utils import *
+from wordcloud import WordCloud
 
 import matplotlib
 matplotlib.use("Agg")
@@ -24,26 +25,30 @@ parser = argparse.ArgumentParser(description='National Library of Finland (NLF) 
 parser.add_argument('--query', default=0, type=int) # smallest
 args = parser.parse_args()
 
-global result_directory
+sz=13 # >>>>>>>>> 12 original <<<<<<<<<<<
+params = {
+	'figure.figsize':	(sz*1.7, sz*1.0),  # W, H
+	'figure.dpi':		200,
+	'figure.autolayout': True,
+	#'figure.constrained_layout.use': True,
+	'legend.fontsize':	sz*0.8,
+	'axes.labelsize':	sz*0.2,
+	'axes.titlesize':	sz*0.2,
+	'xtick.labelsize':	sz*1.0,
+	'ytick.labelsize':	sz*1.0,
+	'lines.linewidth' :	sz*0.1,
+	'lines.markersize':	sz*0.8,
+	'font.size':		sz*1.0,
+	'font.family':		"serif",
+}
+pylab.rcParams.update(params)
 
-def main():
-	print("#"*70)
-	print(f"\t\t\tDATA ANALYSIS")
-	print("#"*70)
-	df = load_df(infile=get_query_dataframe(QUERY=args.query))
-	print(df.shape)
-	cols = list(df.columns)
-	print(len(cols), cols)
-	print("#"*150)
-
-	print(df.head(10))
-	print("-"*150)
-	print(df.tail(10))
-
-	print(df.isna().sum())
-	print("-"*150)
-	print(df.info(verbose=True, memory_usage="deep"))
-	
+sns.set(font_scale=1.3, 
+				style="white", 
+				palette='deep', 
+				font="serif", 
+				color_codes=True,
+				)
 
 def get_query_dataframe(QUERY=0):
 	#print(f"\nGiven log file index: {QUERY}")
@@ -59,6 +64,287 @@ def get_query_dataframe(QUERY=0):
 	print(f"Input Query: {QUERY}")
 
 	return query_dataframe_file
+
+def get_result_directory(QUERY=0):
+	#print(f"\nGiven log file index: {QUERY}")
+	all_dump_file_paths = natsorted( glob.glob( os.path.join(dfs_path, "*.dump") ) )
+	#print(all_dump_file_paths)
+
+	all_files = [dfile[dfile.rfind("/")+1:dfile.rfind(".dump")] for dfile in all_dump_file_paths]
+	#print(all_files)
+
+	query_dataframe_file = all_files[QUERY] 
+	res_dir = os.path.join(rpath, query_dataframe_file)
+
+	return res_dir
+
+def plot_missing_features(df, fname, RES_DIR):
+	print(f">> Visualizing missing data of {fname} ...")
+
+	print(f">>>>> Barplot >>>>>")
+	g = sns.displot(
+			data=df.isna().melt(value_name="Missing"),
+			y="variable",
+			hue="Missing",
+			multiple="stack",
+			height=16,
+			#kde=True,
+			aspect=1.2,
+	)
+	g.set_axis_labels("Samples", "Features")
+	for axb in g.axes.ravel():
+		# add annotations
+		for c in axb.containers:
+			# custom label calculates percent and add an empty string so 0 value bars don't have a number
+			labels = [f"{(v.get_width()/df.shape[0]*100):.1f} %" if v.get_width() > 0 else "" for v in c]
+			axb.bar_label(c,
+										labels=labels,
+										label_type='edge',
+										#fontsize=13,
+										rotation=0,
+										padding=5,
+										)
+			break; # only annotate the first!
+		axb.margins(y=0.3)
+	plt.savefig(os.path.join( RES_DIR, f"{fname}_missing_barplot.png" ), )
+	plt.clf()
+
+	print(f">>>>> Heatmap >>>>>")
+	f, ax = plt.subplots()
+	ax = sns.heatmap(
+			df.isna(),
+			cmap=sns.color_palette("Greys"),
+			cbar_kws={'label': 'NaN (Missing Data):', 'ticks': [0.0, 1.0]},
+			)
+
+	ax.set_ylabel(f"Samples\n\n{df.shape[0]}$\longleftarrow${0}")
+	ax.set_yticks([])
+	ax.xaxis.tick_top()
+	ax.tick_params(axis='x', labelrotation=90)
+	plt.suptitle(f"Missing {fname} Data (NaN)")
+	plt.savefig(os.path.join( RES_DIR, f"{fname}_missing_heatmap.png" ), )
+	plt.clf()
+
+def plot_language(df, fname, RES_DIR, N=10):
+	unq = df["language"].value_counts()
+	print(f">> langueage:\n{unq}")
+
+	df_tmp = df.dropna(axis=0, how="any", subset=["language"]).reset_index(drop=True)
+
+	lu, lc = np.unique(df_tmp["language"], return_counts=True)
+	print(lu.shape[0], lu, lc)
+
+	print(f"\n>> sorting for Top {N} ...")
+	lc_sorted_idx = np.argsort(-lc)
+
+	language_ung = lu[lc_sorted_idx][:N]
+	language_counts = lc[lc_sorted_idx][:N]
+	print(language_ung.shape[0], language_ung, language_counts)
+
+	clrs = ['#1f77b4', 
+					'#cc9911', 
+					'#e377c2', 
+					'#7f7f7f', 
+					'#99ff99',
+					'#ff7f0e', 
+					'#16b3ff',
+					'#9467bd', 
+					'#d62728', 
+					'#0ecd19', 
+					'#ffcc99', 
+					'#bcbd22', 
+					'#ffc9', 
+					'#17becf',
+					'#2ca02c', 
+					'#8c564b', 
+					'#ff9999',
+					]
+
+
+	patches, lbls, pct_texts = plt.pie(language_counts,
+																				labels=language_ung, 
+																				autopct='%1.1f%%', 
+																				#startangle=180,
+																				#radius=3, USELESS IF axs[0].axis('equal')
+																				#pctdistance=1.5,
+																				#labeldistance=0.5,
+																				rotatelabels=True,
+																				#counterclock=False,
+																				colors=clrs,
+																				)
+	for lbl, pct_text in zip(lbls, pct_texts):
+		pct_text.set_rotation(lbl.get_rotation())
+
+	plt.title(f"Top {N} Searched Languages in NLF")
+	plt.savefig(os.path.join( RES_DIR, f"{fname}_pie_chart_language.png" ), )
+	plt.clf()
+
+def plot_word(df, fname, RES_DIR):
+	unq = df["query_word"].value_counts()
+	print(f">> query_word:\n{unq}")
+
+	wordcloud = WordCloud(width=800, 
+												height=400, 
+												background_color="black",
+												colormap="RdYlGn",
+												max_font_size=50, 
+												stopwords=None,
+												repeat= True).generate(df["query_word"].str.cat(sep=","))
+
+	plt.figure(figsize = (20, 8),facecolor = "#ffd100") 
+	plt.imshow(wordcloud)
+	plt.axis("off")
+	plt.margins(x = 0, y = 0)
+	plt.tight_layout(pad = 0)
+	plt.savefig(os.path.join( RES_DIR, f"{fname}_query_words_cloud.png" ), )
+	plt.clf()
+
+def plot_ocr_term(df, fname, RES_DIR):
+	unq = df["ocr_term"].value_counts()
+	print(f">> ocr_term:\n{unq}")
+
+	wordcloud = WordCloud(width=800, 
+												height=400, 
+												background_color="black",
+												colormap="RdYlGn",
+												max_font_size=50, 
+												stopwords=None,
+												repeat= True).generate(df["ocr_term"].str.cat(sep=", | , | ,"))
+
+	plt.figure(figsize = (20, 8),facecolor = "#ffd100") 
+	plt.imshow(wordcloud)
+	plt.axis("off")
+	plt.margins(x = 0, y = 0)
+	plt.tight_layout(pad = 0)
+	plt.savefig(os.path.join( RES_DIR, f"{fname}_OCR_terms_cloud.png" ), )
+	plt.clf()
+
+
+def plot_doc_type(df, fname, RES_DIR):
+	unq = df["document_type"].value_counts()
+	print(f">> doc_type:\n{unq}")
+
+	wordcloud = WordCloud(width=800, 
+												height=400, 
+												background_color="black",
+												colormap="RdYlGn",
+												max_font_size=50, 
+												stopwords=None,
+												repeat= True).generate(df["document_type"].str.cat(sep=","))
+
+	plt.figure(figsize = (20, 8),facecolor = "#ffd100") 
+	plt.imshow(wordcloud)
+	plt.axis("off")
+	plt.margins(x = 0, y = 0)
+	plt.tight_layout(pad = 0)
+	plt.savefig(os.path.join( RES_DIR, f"{fname}_doc_type_cloud.png" ), )
+	plt.clf()
+
+def plot_user(df, fname, RES_DIR, N=10):
+	unq = df["user_ip"].value_counts()
+	print(f">> user_ip:\n{unq}")
+
+	df_tmp = df.dropna(axis=0, how="any", subset=["user_ip"]).reset_index(drop=True)
+
+	lu, lc = np.unique(df_tmp["user_ip"], return_counts=True)
+	print(lu.shape[0], lu, lc)
+
+	print(f"\n>> sorting for Top {N} users:")
+	lc_sorted_idx = np.argsort(-lc)
+	language_ung = lu[lc_sorted_idx][:N]
+	language_counts = lc[lc_sorted_idx][:N]
+	print(language_ung.shape[0], language_ung, language_counts)
+	#return
+
+
+	plt.subplots()
+	palette = ["#1d7874",
+						"#679289",
+						"#f4c095",
+						"#ee2e31",
+						"#ffb563",
+						"#918450",
+						"#f85e00",
+						"#a41623",
+						"#9a031e",
+						"#d6d6d6",
+						"#ffee32",
+						"#ffd100",
+						"#333533",
+						"#202020",
+						]
+	p = sns.barplot(x=language_ung,
+									y=language_counts,
+									palette=palette, 
+									saturation=1, 
+									edgecolor = "#1c1c1c",
+									linewidth = 2,
+									)
+
+	p.axes.set_title(f"\nTop {N} Users\n", fontsize=25)
+	plt.ylabel("Counts" , fontsize = 20)
+	plt.xlabel("\nUser Name" , fontsize = 20)
+	# plt.yscale("log")
+	plt.xticks(rotation = 90)
+	for container in p.containers:
+			p.bar_label(container,
+									label_type = "center",
+									padding = 6,
+									size = 15,
+									color = "black",
+									rotation = 90,
+									bbox={"boxstyle": "round", 
+												"pad": 0.6, 
+												"facecolor": "orange", 
+												"edgecolor": "black", 
+												"alpha": 1,
+												}
+									)
+
+	sns.despine(left=True, bottom=True)
+	plt.savefig(os.path.join( RES_DIR, f"{fname}_top_{N}_users.png" ), )
+	plt.clf()
+
+def main():
+	print("#"*70)
+	print(f"\t\t\tDATA ANALYSIS")
+	print("#"*70)
+
+	result_directory = get_result_directory(QUERY=args.query)
+	print(result_directory)
+	make_folder(folder_name=result_directory)
+
+	QUERY_FILE = get_query_dataframe(QUERY=args.query)
+	df = load_df(infile=QUERY_FILE)
+
+	print(df.shape)
+	cols = list(df.columns)
+	print(len(cols), cols)
+	print("#"*150)
+
+	print(df.head(10))
+	print("-"*150)
+	print(df.tail(10))
+
+	print(df.isna().sum())
+	print("-"*150)
+	print(df.info(verbose=True, memory_usage="deep"))
+
+	# missing features:
+	plot_missing_features(df, fname=QUERY_FILE, RES_DIR=result_directory)
+
+	# users:
+	plot_user(df, fname=QUERY_FILE, RES_DIR=result_directory)
+
+	# language
+	plot_language(df, fname=QUERY_FILE, RES_DIR=result_directory)
+
+	# publication
+	plot_doc_type(df, fname=QUERY_FILE, RES_DIR=result_directory)
+
+	# query words & terms:
+	plot_word(df, fname=QUERY_FILE, RES_DIR=result_directory)
+	plot_ocr_term(df, fname=QUERY_FILE, RES_DIR=result_directory)
 
 if __name__ == '__main__':
 	os.system('clear')
