c57daa1b3c9b04bf6c16e1f6e08f163fbd4b2c68
xenial <farid.alijani@gmail.com>
2023-04-17 12:35:41 +0300

    stanza full bows nikeX print docs in tokenizer

--- tokenizer_utils.py
+++ tokenizer_utils.py
@@ -75,7 +75,7 @@ def spacy_tokenizer(sentence):
 	return lematized_tokens
 
 def stanza_lemmatizer(docs):
-	#print(f'Raw inp: (len: {len(docs)}) >>{docs}<<', end='\t')
+	print(f'Raw inp: (len: {len(docs)}) >>{docs}<<', end='\t')
 	if not docs:
 		return
 
@@ -84,12 +84,12 @@ def stanza_lemmatizer(docs):
 	if not docs:
 		return
 
-	#print(f'preprocessed: >>{docs}<<', end='\t')
+	print(f'preprocessed: >>{docs}<<', end='\t')
 	all_ = stanza_multi_pipeline(docs)
 	#lm = [ word.lemma.lower() for i, sent in enumerate(all_.sentences) for word in sent.words if ( word.lemma and len(re.sub(r'[A-Za-z][.][\s]+|[A-Za-z][.]+|\b[A-Za-z][\s]+', '', word.lemma ) ) > 2 and word.pos not in useless_upos_tags ) ]
 	lm = [ re.sub('#|_','', word.lemma.lower()) for i, sent in enumerate(all_.sentences) for word in sent.words if ( word.lemma and len(re.sub(r'[A-Za-z][.][\s]+|[A-Za-z][.]+|\b[A-Za-z][\s]+', '', word.lemma ) ) > 2 and word.pos not in useless_upos_tags ) ]
 
-	#print( list( set( lm ) ) )
+	print( list( set( lm ) ) )
 	return list( set( lm ) )
 
 def trankit_lemmatizer(docs):
