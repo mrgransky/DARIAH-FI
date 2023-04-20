498a1482034daa4e2bc72c8dd47bb0974bd39b84
xenial <farid.alijani@gmail.com>
2023-04-20 13:28:55 +0300

    no ax bar_label

--- RecSys_usr_token.py
+++ RecSys_usr_token.py
@@ -843,10 +843,12 @@ def plot_tokens_by(userIP, tks_name, tks_value_all, tks_value_separated, topTKs=
 	ax.spines[['top', 'right']].set_visible(False)
 	ax.legend(loc="best", fontsize=8.0)
 
+	"""
 	for bar in hbars:
 		filtered_lbls = [f"{v:.1f}" if v>=0.8 else "" for v in bar.datavalues]
 		ax.bar_label(bar, labels=filtered_lbls, label_type='center', rotation=0.0, fontsize=6.0)
-
+	"""
+	
 	"""
 	for c in ax.containers:
 		print(f">> container: {c}")
@@ -858,8 +860,8 @@ def plot_tokens_by(userIP, tks_name, tks_value_all, tks_value_separated, topTKs=
 									label_type='edge',
 								)
 	"""
-	#plt.savefig(os.path.join( RES_DIR, f"qu_{args.qphrase.replace(' ', '_')}_usr_{userIP}_topTKs{nTokens}_seperated_{sp_type}_SP.png" ), bbox_inches='tight')
-	plt.savefig(os.path.join( RES_DIR, f"qu_{args.qphrase.replace(' ', '_')}_usr_{userIP}_topTKs{nTokens}_sep_{sp_type}_SP.png" ))
+	plt.savefig(os.path.join( RES_DIR, f"qu_{args.qphrase.replace(' ', '_')}_usr_{userIP}_topTKs{nTokens}_seperated_{sp_type}_SP.png" ), bbox_inches='tight')
+	#plt.savefig(os.path.join( RES_DIR, f"qu_{args.qphrase.replace(' ', '_')}_usr_{userIP}_topTKs{nTokens}_sep_{sp_type}_SP.png" ))
 
 	plt.clf()
 	plt.close(f)
