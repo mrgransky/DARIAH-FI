862846c5b278e21f0020ca2d6f92696b7d398046
xenial <farid.alijani@gmail.com>
2023-04-23 20:34:34 +0300

    full bow nikeX hugemmem 950G

--- puhti_sbatch_RecSys.sh
+++ puhti_sbatch_RecSys.sh
@@ -4,7 +4,7 @@
 #SBATCH -J nikeX_stanzaTK_full_bow
 #SBATCH -o /scratch/project_2004072/Nationalbiblioteket/trash/NLF_logs/%x_%N_%j.out
 #SBATCH --partition=hugemem
-#SBATCH --mem-per-cpu=760G
+#SBATCH --mem-per-cpu=950G
 #SBATCH --time=02-23:59:59
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=ALL
