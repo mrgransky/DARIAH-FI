21f950475ab07f55a2eb6cd6c71b75decce49b80
xenial <farid.alijani@gmail.com>
2023-04-24 16:05:18 +0300

    init abb check hugemem longrun cpu

--- puhti_sbatch_RecSys.sh
+++ puhti_sbatch_RecSys.sh
@@ -3,9 +3,9 @@
 #SBATCH --account=project_2004072
 #SBATCH -J nikeX_stanzaTK
 #SBATCH -o /scratch/project_2004072/Nationalbiblioteket/trash/NLF_logs/%x_%N_%j.out
-#SBATCH --partition=hugemem
+#SBATCH --partition=hugemem_longrun
 #SBATCH --mem-per-cpu=366G
-#SBATCH --time=02-23:59:59
+#SBATCH --time=12-23:59:59
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --cpus-per-task=1
