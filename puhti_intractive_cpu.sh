07a6857ff8806a71f19d4abdca4eadfdbffaa435
xenial <farid.alijani@gmail.com>
2023-05-02 08:27:51 +0300

    intractive cpu 32G mem

--- puhti_intractive_cpu.sh
+++ puhti_intractive_cpu.sh
@@ -7,7 +7,7 @@
 
 # Image_Retrieval_TUNI
 #srun -J cpu_350gb --account=project_2004072 --partition=hugemem --time=1-10:59:00 --mem=999G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
-srun -J intrtv_cpu --account=project_2004072 --partition=small --time=0-05:59:00 --mem=26G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J intrtv_cpu --account=project_2004072 --partition=small --time=0-23:59:00 --mem=32G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 
 # APP_CSC
 #srun -J v100_intrc --account=Project_2004160 --partition=hugemem_longrun --time=5-23:59:00 --mem=512G --ntasks=1 --pty /bin/bash -i
