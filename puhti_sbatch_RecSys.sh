7fbc815ffbd09230e38adbe63de0332ce8e65f67
xenial <farid.alijani@gmail.com>
2023-05-11 01:40:01 +0300

    stanza dfs unique nikeX hugemem longrun 1 cpu/task

--- puhti_sbatch_RecSys.sh
+++ puhti_sbatch_RecSys.sh
@@ -7,14 +7,14 @@
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=5
-#SBATCH --time=02-23:59:59
+#SBATCH --cpus-per-task=1
+#SBATCH --time=12-23:59:59
 
 # # # # # # SBATCH --partition=gpu
 # # # # # # SBATCH --gres=gpu:v100:1
 # # # # # # SBATCH --mem=24G
 
-#SBATCH --partition=hugemem
+#SBATCH --partition=longrun_hugemem
 #SBATCH --mem-per-cpu=486G
 
 # # # # # # SBATCH --array=0-69
