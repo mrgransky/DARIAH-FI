603229649ece6a5a903b85bdce165d21d5f4ba3c
xenial <farid.alijani@gmail.com>
2023-05-15 11:18:15 +0300

    chck trankit lemma

--- tokenizer_utils.py
+++ tokenizer_utils.py
@@ -90,27 +90,27 @@ def stanza_lemmatizer(docs):
 	all_ = stanza_multi_pipeline(docs)
 	lm = [ re.sub('#|_','', word.lemma.lower()) for i, sent in enumerate(all_.sentences) for word in sent.words if ( word.lemma and len( re.sub(r'\b[A-Z](\.| |\:)+|\b[a-z](\.| |\:)+', '', word.lemma ) ) > 2 and word.upos not in useless_upos_tags and word.lemma.lower() not in UNIQUE_STOPWORDS ) ]
 
-	#print( list( set( lm ) ) )
+	#print( lm )
 	#return list( set( lm ) )
 	return lm
 
 def trankit_lemmatizer(docs):
-	#print(f'Raw inp ({len(docs)}): >>{docs}<<', end='\t')
+	print(f'Raw: (len: {len(docs)}) >>{docs}<<', end='\t')
 	if not docs:
 		return
 
 	# treat all as document
 	docs = re.sub(r'"|<.*?>|[~|*|^][\d]+', '', docs)
 	docs = re.sub(r'[+|*|"|^|~|.|(|)|”|“|:]', ' ', docs ).strip()
-	#print(f'preprocessed: {len(docs)} >>{docs}<<')
+	
+	print(f'preprocessed: (len: {len(docs)}) >>{docs}<<', end='\t')
 	if ( not docs or len(docs)==0 ):
 		return
 
 	all_dict = p(docs)
 	lm = [ tk.get("lemma").lower() for sent in all_dict.get("sentences") for tk in sent.get("tokens") if ( tk.get("lemma") and len(re.sub(r'\b[A-Z](\.| |\:)+|\b[a-z](\.| |\:)+', '', tk.get("lemma") ) ) > 2 and tk.get("upos") not in useless_upos_tags and tk.get("lemma").lower() not in UNIQUE_STOPWORDS ) ] 
 
-	#print( list( set( lm ) ) )
-	#return list( set( lm ) )
+	print( lm )
 	return lm
 
 def nltk_lemmatizer(sentence):	
