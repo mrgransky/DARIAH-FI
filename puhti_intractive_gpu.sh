950f6270f21b6aa620a511d9dc096b45dfda845f
xenial <farid.alijani@gmail.com>
2023-05-16 13:29:30 +0300

    64gb gpu interactive

--- puhti_intractive_gpu.sh
+++ puhti_intractive_gpu.sh
@@ -9,7 +9,7 @@
 # Image_Retrieval_TUNI
 
 #srun -J v100_32gb --account=project_2004072 --partition=gpu --gres=gpu:v100:1 --time=0-10:59:00 --mem-per-cpu=22G --ntasks=1 --cpus-per-task=1 nvidia-smi
-srun -J v100_interactive --account=project_2004072 --partition=gpu --gres=gpu:v100:1 --time=0-03:59:00 --mem-per-cpu=26G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J v100_interactive --account=project_2004072 --partition=gpu --gres=gpu:v100:1 --time=0-03:59:00 --mem-per-cpu=64G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 
 # APP_CSC
 #srun -J v100_intrc --account=Project_2004160 --partition=gpu --gres=gpu:v100:1 --time=1-23:59:00 --mem=128G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
