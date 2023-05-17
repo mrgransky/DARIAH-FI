3ea7d0d3e16b5d35e49ab8cb04116d7a1831d431
xenial <farid.alijani@gmail.com>
2023-05-17 12:27:42 +0300

    small partition 64g cpu

--- puhti_sbatch_RecSys.sh
+++ puhti_sbatch_RecSys.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 #SBATCH --account=project_2004072
-#SBATCH -J nikeX_stanzaTK_complete
+#SBATCH -J nikeY_stanzaTK_complete
 #SBATCH -o /scratch/project_2004072/Nationalbiblioteket/trash/NLF_logs/%x_%N_%j.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=13-23:59:59
+#SBATCH --time=02-23:59:59
 
 # # # # SBATCH --partition=gpu
 # # # # SBATCH --gres=gpu:v100:1
 # # # # SBATCH --mem=46G
 
-#SBATCH --partition=hugemem_longrun
-#SBATCH --mem-per-cpu=584G
+#SBATCH --partition=small
+#SBATCH --mem-per-cpu=64G
 
 # # # # # # SBATCH --array=0-69
 # # # # # # SBATCH -o NLF_logs/q%a_%x_%N_%j.out
@@ -43,7 +43,7 @@ elif [ $user == 'alijanif' ]; then
 	dfs_dir="/scratch/project_2004072/Nationalbiblioteket/datasets"
 fi
 
-python -u RecSys_usr_token.py --inputDF $dfs_dir/nikeX.docworks.lib.helsinki.fi_access_log.07_02_2021.log.dump --lmMethod 'stanza'
+python -u RecSys_usr_token.py --inputDF $dfs_dir/nikeY.docworks.lib.helsinki.fi_access_log.07_02_2021.log.dump --lmMethod 'stanza'
 
 done_txt="SLURM JOB ENDED AT: `date`"
 echo -e "${done_txt//?/$ch}\n${done_txt}\n${done_txt//?/$ch}"
