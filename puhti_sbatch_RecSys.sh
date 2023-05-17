a2b56311fb38d2146a0fa4f4d6d31c19c1d17df8
xenial <farid.alijani@gmail.com>
2023-05-17 21:51:19 +0300

    check avgrec debug sbatch cpu nikeY small partition

--- puhti_sbatch_RecSys.sh
+++ puhti_sbatch_RecSys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --account=project_2004072
-#SBATCH -J nikeY_stanzaTK_debug
+#SBATCH -J nikeX_stanzaTK_debug
 #SBATCH -o /scratch/project_2004072/Nationalbiblioteket/trash/NLF_logs/%x_%N_%j.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=ALL
@@ -14,8 +14,8 @@
 # # # # SBATCH --gres=gpu:v100:1
 # # # # SBATCH --mem=51G
 
-#SBATCH --partition=hugemem
-#SBATCH --mem-per-cpu=374G
+#SBATCH --partition=small
+#SBATCH --mem-per-cpu=64G
 
 # # # # # # SBATCH --array=0-69
 # # # # # # SBATCH -o NLF_logs/q%a_%x_%N_%j.out
@@ -43,7 +43,7 @@ elif [ $user == 'alijanif' ]; then
 	dfs_dir="/scratch/project_2004072/Nationalbiblioteket/datasets"
 fi
 
-python -u RecSys_usr_token.py --inputDF $dfs_dir/nikeY.docworks.lib.helsinki.fi_access_log.07_02_2021.log.dump --lmMethod 'stanza' --qphrase 'stockholms universitet'
+python -u RecSys_usr_token.py --inputDF $dfs_dir/nikeX.docworks.lib.helsinki.fi_access_log.07_02_2021.log.dump --lmMethod 'stanza' --qphrase 'älgjakt jakobstad'
 
 done_txt="SLURM JOB ENDED AT: `date`"
 echo -e "${done_txt//?/$ch}\n${done_txt}\n${done_txt//?/$ch}"
