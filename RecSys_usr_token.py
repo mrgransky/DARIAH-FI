9556da3a699faf8ef334a7371ff1cbb9f71f3952
xenial <farid.alijani@gmail.com>
2023-05-19 00:49:15 +0300

    check sum with small bow

--- RecSys_usr_token.py
+++ RecSys_usr_token.py
@@ -261,35 +261,35 @@ def sum_all_tokens_appearance_in_vb(dframe, weights: List[float], vb: Dict[str, 
 			print(f"\tprev_vb[{q_tk}]={updated_vocab.get(q_tk)}")
 			updated_vocab[q_tk] = updated_vocab.get(q_tk) + w_qu
 			print(f"\tcurr_vb[{q_tk}]={updated_vocab.get(q_tk)}")
-			
+	print("#"*80)
 	for i, sn_hw_tk in enumerate(dframe.snippets_hw_token):
 		print(f"<snHW[{i}]: {sn_hw_tk}> w: {w_hw_sn} | exists in vb: {updated_vocab.get(sn_hw_tk) is not None} ")
 		if updated_vocab.get(sn_hw_tk) is not None:
 			print(f"\tprev_vb[{sn_hw_tk}]={updated_vocab.get(sn_hw_tk)}")
 			updated_vocab[sn_hw_tk] = updated_vocab.get(sn_hw_tk) + w_hw_sn
 			print(f"\tcurr_vb[{sn_hw_tk}]={updated_vocab.get(sn_hw_tk)}")
-
+	print("#"*80)
 	for i, sn_tk in enumerate(dframe.snippets_token):
 		print(f"<sn[{i}]: {sn_tk}> w: {w_sn} | exists in vb: {updated_vocab.get(sn_tk) is not None} ")
 		if updated_vocab.get(sn_tk) is not None:
 			print(f"\tprev_vb[{sn_tk}]={updated_vocab.get(sn_tk)}")
 			updated_vocab[sn_tk] = updated_vocab.get(sn_tk) + w_sn
 			print(f"\tcurr_vb[{sn_tk}]={updated_vocab.get(sn_tk)}")
-
+	print("#"*80)
 	for i, c_hw_tk in enumerate(dframe.nwp_content_hw_token):
 		print(f"<cntHW[{i}]: {c_hw_tk}> w: {w_hw_cnt} | exists in vb: {updated_vocab.get(c_hw_tk) is not None} ")
 		if updated_vocab.get(c_hw_tk) is not None:
 			print(f"\tprev_vb[{c_hw_tk}]={updated_vocab.get(c_hw_tk)}")
 			updated_vocab[c_hw_tk] = updated_vocab.get(c_hw_tk) + w_hw_cnt
 			print(f"\tcurr_vb[{c_hw_tk}]={updated_vocab.get(c_hw_tk)}")
-
+	print("#"*80)
 	for i, c_pt_tk in enumerate(dframe.nwp_content_pt_token):
 		print(f"<cntPT[{i}]: {c_pt_tk}> w: {w_pt_cnt} | exists in vb: {updated_vocab.get(c_pt_tk) is not None} ")
 		if updated_vocab.get(c_pt_tk) is not None:
 			print(f"\tprev_vb[{c_pt_tk}]={updated_vocab.get(c_pt_tk)}")
 			updated_vocab[c_pt_tk] = updated_vocab.get(c_pt_tk) + w_pt_cnt
 			print(f"\tcurr_vb[{c_pt_tk}]={updated_vocab.get(c_pt_tk)}")
-
+	print("#"*80)
 	for i, c_tk in enumerate(dframe.nwp_content_lemma_all):
 		print(f"<cnt[{i}]: {c_tk}> w: {w_cnt} | exists in vb: {updated_vocab.get(c_tk) is not None} ")
 		if updated_vocab.get(c_tk) is not None:
@@ -580,14 +580,14 @@ def get_sparse_matrix(df):
 
 def run_RecSys(df_inp, qu_phrase, topK=5, normalize_sp_mtrx=False, ):
 	print(f">> Running {__file__} with {args.lmMethod.upper()} lemmatizer")
-	
+	"""
 	if userName.endswith("xenial"):
 		BoWs = get_BoWs(dframe=df_inp)
 	else:
 		BoWs = get_cBoWs(dframe=df_inp)
 	#return
-	
-	#BoWs = get_BoWs(dframe=df_inp)
+	"""
+	BoWs = get_BoWs(dframe=df_inp)
 	#BoWs = get_cBoWs(dframe=df_inp)
 	
 	try:
