28fa906bf13197c258dcd4222d2f0fdc995f3629
xenial <farid.alijani@gmail.com>
2023-05-19 13:28:06 +0300

    nikeX small bow for checking sparse matrix

--- tokenizer_utils.py
+++ tokenizer_utils.py
@@ -67,22 +67,22 @@ def spacy_tokenizer(sentence):
 	return lematized_tokens
 
 def stanza_lemmatizer(docs):
-	print(f'Raw: (len: {len(docs)}) >>{docs}<<')
+	#print(f'Raw: (len: {len(docs)}) >>{docs}<<')
 	if not docs:
 		return
 	# treat all as document
 	docs = re.sub(r'"|<.*?>|[~|*|^][\d]+', '', docs)
 	docs = re.sub(r'[%|+|,|;|=|&|\'|*|"|^|~|?|!|—|.|°|•||\(\)|\”\“|:|/|‘|’|<|>|»|„|«|♦|■|\\|\[\]|-]+', ' ', docs ).strip()
 
-	print(f'preprocessed: (len: {len(docs)}) >>{docs}<<')
+	#print(f'preprocessed: (len: {len(docs)}) >>{docs}<<')
 	if ( not docs or len(docs)==0 ):
 		return
 
 	all_ = stanza_multi_pipeline(docs)
 	lm = [ re.sub('#|_','', word.lemma.lower()) for i, sent in enumerate(all_.sentences) for word in sent.words if ( word.lemma and len( re.sub(r'\b[A-Z](\.| |\:)+|\b[a-z](\.| |\:)+', '', word.lemma ) ) > 2 and word.upos not in useless_upos_tags and word.lemma.lower() not in UNIQUE_STOPWORDS ) ]
 
-	print( lm )
-	print("<>"*70)
+	#print( lm )
+	#print("<>"*70)
 	return lm
 
 def trankit_lemmatizer(docs):
