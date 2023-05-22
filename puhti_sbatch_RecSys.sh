91ba41670635e9fa4d8f1256406165f871541a5c
xenial <farid.alijani@gmail.com>
2023-05-22 18:56:43 +0300

    cbow results raw kantakirjasonni

--- puhti_sbatch_RecSys.sh
+++ puhti_sbatch_RecSys.sh
@@ -42,7 +42,7 @@ elif [ $user == 'alijanif' ]; then
 	dfs_dir="/scratch/project_2004072/Nationalbiblioteket/datasets"
 fi
 
-python -u RecSys_usr_token.py --inputDF $dfs_dir/nikeX.docworks.lib.helsinki.fi_access_log.07_02_2021.log.dump --lmMethod 'stanza' --qphrase 'Stockholms universitet'
+python -u RecSys_usr_token.py --inputDF $dfs_dir/nikeX.docworks.lib.helsinki.fi_access_log.07_02_2021.log.dump --lmMethod 'stanza' --qphrase 'kantakirjasonni'
 
 done_txt="SLURM JOB ENDED AT: `date`"
 echo -e "${done_txt//?/$ch}\n${done_txt}\n${done_txt//?/$ch}"
