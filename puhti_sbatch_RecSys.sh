a4efb8390e36326268d06e179a9e066c11eedda0
xenial <farid.alijani@gmail.com>
2023-05-24 14:41:05 +0300

    run with sbatch rename jobsname gpu 96g

--- puhti_sbatch_RecSys.sh
+++ puhti_sbatch_RecSys.sh
@@ -12,7 +12,7 @@
 # # # # SBATCH --partition=small
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
-#SBATCH --mem=51G
+#SBATCH --mem=96G
 
 # # # # # # SBATCH --array=0-69
 # # # # # # SBATCH -o NLF_logs/q%a_%x_%N_%j.out
