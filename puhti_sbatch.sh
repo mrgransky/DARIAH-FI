3456c5cb5c073af9cf1fd22a8739ea6bbeeac8f2
xenial <farid.alijani@gmail.com>
2023-05-24 15:57:35 +0300

    for all log files in array 0-500 check

--- puhti_sbatch.sh
+++ puhti_sbatch.sh
@@ -9,22 +9,22 @@
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --cpus-per-task=1
-#SBATCH --array=0-1096
-
+#SBATCH --array=0-500
+# # # # array: 0-1096
 stars=$(printf '%*s' 90 '')
 txt="SLURM JOB STARTED AT: `date`"
 ch="#"
 echo -e "${txt//?/$ch}\n${txt}\n${txt//?/$ch}"
 echo "${stars// /*}"
 echo "HOST: $SLURM_SUBMIT_HOST, CLUSTER: $SLURM_CLUSTER_NAME, WRK_DIR: $SLURM_SUBMIT_DIR"
-echo "JOB: name: $SLURM_JOB_NAME, ID: $SLURM_JOB_ID, Array_ID: $SLURM_ARRAY_JOB_ID"
+echo "JOB: name: $SLURM_JOB_NAME, ID: $SLURM_JOB_ID"
 echo "NUM_NODES: $SLURM_JOB_NUM_NODES, NODELIST: $SLURM_JOB_NODELIST, NODE_ID: $SLURM_NODEID"
 echo "NTASKS: $SLURM_NTASKS, PROCID: $SLURM_PROCID"
 echo "CPUS_ON_NODE: $SLURM_CPUS_ON_NODE, CPUS/TASK: $SLURM_CPUS_PER_TASK, MEM/CPU: $SLURM_MEM_PER_CPU"
 echo "${stars// /*}"
 
 user="`whoami`"
-echo "Cluster: $SLURM_CLUSTER_NAME | User: $user | Array: $SLURM_ARRAY_TASK_ID"
+echo "Cluster: $SLURM_CLUSTER_NAME | $user | arrayTaskID: $SLURM_ARRAY_TASK_ID | arrayJobID: $SLURM_ARRAY_JOB_ID"
 
 if [ $user == 'alijani' ]; then
 	source activate py3_gpu
