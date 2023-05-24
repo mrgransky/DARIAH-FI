f2466a0a75a472875b317928e628c652bcd7d7f4
xenial <farid.alijani@gmail.com>
2023-05-24 23:23:51 +0300

    narvi sbatch

---
+++ narvi_sbatch.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH -J dfQ
+#SBATCH -o /lustre/sgn-data/vision/trash_log/NLF_logs/%x_%a_%N_%j_%A.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=6G
+#SBATCH --time=6-23:59:59
+#SBATCH --partition=normal
+#SBATCH --mail-user=farid.alijani@gmail.com
+#SBATCH --mail-type=ALL
+# # # # # # SBATCH --gres=gpu:teslav100:1
+#SBATCH --array=0-1096
+
+stars=$(printf '%*s' 90 '')
+txt="SLURM JOB STARTED AT: `date`"
+ch="#"
+echo -e "${txt//?/$ch}\n${txt}\n${txt//?/$ch}"
+echo "${stars// /*}"
+echo "HOST: $SLURM_SUBMIT_HOST, CLUSTER: $SLURM_CLUSTER_NAME, WRK_DIR: $SLURM_SUBMIT_DIR"
+echo "JOB: name: $SLURM_JOB_NAME, ID: $SLURM_JOB_ID"
+echo "NUM_NODES: $SLURM_JOB_NUM_NODES, NODELIST: $SLURM_JOB_NODELIST, NODE_ID: $SLURM_NODEID"
+echo "NTASKS: $SLURM_NTASKS, PROCID: $SLURM_PROCID"
+echo "CPUS_ON_NODE: $SLURM_CPUS_ON_NODE, CPUS/TASK: $SLURM_CPUS_PER_TASK, MEM/CPU: $SLURM_MEM_PER_CPU"
+echo "${stars// /*}"
+
+user="`whoami`"
+echo "Cluster: $SLURM_CLUSTER_NAME | $user | arrayTaskID: $SLURM_ARRAY_TASK_ID | arrayJobID: $SLURM_ARRAY_JOB_ID"
+
+if [ $user == 'alijani' ]; then
+	source activate py3_gpu
+elif [ $user == 'alijanif' ]; then
+	echo ">> Using Puhti Conda Environment..."
+	#source /projappl/project_2004072/miniconda3/bin/activate py3_gpu
+fi
+
+python -u nationalbiblioteket_logs.py --saveDF True --query $SLURM_ARRAY_TASK_ID
+
+done_txt="SLURM JOB ENDED AT: `date`"
+echo -e "${done_txt//?/$ch}\n${done_txt}\n${done_txt//?/$ch}"
+echo "${stars// /*}"
---
+++ narvi_sbatch_RecSys.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#SBATCH --account=project_2004072
+#SBATCH -J df_concat_stanzaTK_cBoW_su
+#SBATCH -o /scratch/project_2004072/Nationalbiblioteket/trash/NLF_logs/%x_%N_%j.out
+#SBATCH --mail-user=farid.alijani@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=02-23:59:59
+# # # # SBATCH --partition=small
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH --mem=96G
+
+# # # # # # SBATCH --array=0-69
+# # # # # # SBATCH -o NLF_logs/q%a_%x_%N_%j.out
+
+stars=$(printf '%*s' 90 '')
+txt="SLURM JOB STARTED AT: `date`"
+ch="#"
+echo -e "${txt//?/$ch}\n${txt}\n${txt//?/$ch}"
+echo "${stars// /*}"
+echo "HOST: $SLURM_SUBMIT_HOST, CLUSTER: $SLURM_CLUSTER_NAME, WRK_DIR: $SLURM_SUBMIT_DIR"
+echo "JOB: name: $SLURM_JOB_NAME, ID: $SLURM_JOB_ID, Array_ID: $SLURM_ARRAY_JOB_ID"
+echo "NUM_NODES: $SLURM_JOB_NUM_NODES, NODELIST: $SLURM_JOB_NODELIST, NODE_ID: $SLURM_NODEID"
+echo "NTASKS: $SLURM_NTASKS, PROCID: $SLURM_PROCID"
+echo "CPUS_ON_NODE: $SLURM_CPUS_ON_NODE, CPUS/TASK: $SLURM_CPUS_PER_TASK, MEM/CPU: $SLURM_MEM_PER_CPU"
+echo "${stars// /*}"
+
+user="`whoami`"
+cluster="$SLURM_CLUSTER_NAME"
+echo "Cluster: $cluster Current User: $user"
+
+if [ $user == 'alijani' ]; then
+	source activate py3_gpu
+elif [ $user == 'alijanif' ]; then
+	echo ">> Using Puhti conda env from tykky module..."
+	dfs_dir="/scratch/project_2004072/Nationalbiblioteket/datasets"
+fi
+
+#python -u RecSys_usr_token.py --inputDF $dfs_dir/nikeX.docworks.lib.helsinki.fi_access_log.07_02_2021.log.dump --lmMethod 'stanza' --qphrase 'Stockholms universitet'
+python -u tkRecSys.py --dsPath $dfs_dir --lmMethod 'stanza' --qphrase 'Stockholms universitet'
+
+done_txt="SLURM JOB ENDED AT: `date`"
+echo -e "${done_txt//?/$ch}\n${done_txt}\n${done_txt//?/$ch}"
+echo "${stars// /*}"
