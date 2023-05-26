412392a7d37bf22d0cc319975a278ef4fed38cb9
xenial <farid.alijani@gmail.com>
2023-05-26 11:40:05 +0300

    narvi for all 1096

--- narvi_sbatch.sh
+++ narvi_sbatch.sh
@@ -3,7 +3,7 @@
 #SBATCH -J dfQ
 #SBATCH -o /lustre/sgn-data/Nationalbiblioteket/trash/NLF_logs/%x_%a_%N_%j_%A.out
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8G
+#SBATCH --mem=6G
 #SBATCH --time=6-23:59:59
 #SBATCH --partition=normal
 #SBATCH --mail-user=farid.alijani@gmail.com
