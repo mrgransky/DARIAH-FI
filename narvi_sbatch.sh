5dfc8880918014c37bbb50f41c69f6bbe7dd04e3
xenial <farid.alijani@gmail.com>
2023-06-03 18:01:19 +0300

    60-69 narvi

--- narvi_sbatch.sh
+++ narvi_sbatch.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=ALL
 # # # # # # SBATCH --gres=gpu:teslav100:1
-#SBATCH --array=50-59
+#SBATCH --array=60-69
 
 stars=$(printf '%*s' 90 '')
 txt="SLURM JOB STARTED AT: `date`"
