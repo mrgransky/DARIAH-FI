1cc2b209de3ee7ac01aa506bead5bf4169542437
xenial <farid.alijani@gmail.com>
2023-06-03 21:27:03 +0300

    narvi intractive cpu

---
+++ narvi_intractive_cpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# to run this file:
+# $ bash puhti_intractive_cpu.sh
+# OR ...
+# $ source puhti_intractive_cpu.sh
+
+srun -J cpu_hm  --partition=grid --time=01-23:59:00 --mem=2G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
