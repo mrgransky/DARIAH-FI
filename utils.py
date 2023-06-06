bc38a8cee887c5341f3cc57c36e6abb277bc905b
xenial <farid.alijani@gmail.com>
2023-06-06 19:55:13 +0300

    narvi 86 type issues dtypes

--- utils.py
+++ utils.py
@@ -628,8 +628,9 @@ def get_df_pseudonymized_logs(infile="", TIMESTAMP=None):
 	df['prev_time'] = df.groupby(['referer','user_ip'])['timestamp'].shift()
 	print(df.info())
 	print("<>"*50)
+	print(df.dtypes)
 	th = datetime.timedelta(days=0, seconds=0, minutes=5)
-	print(th, th.dtype, type(th))
+	print(th, type(th))
 	df = df[df['prev_time'].isnull() | df['timestamp'].sub(df['prev_time']).gt(th)]
 	df = df.drop(['prev_time'], axis=1)
 
