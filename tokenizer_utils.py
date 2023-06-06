c4a1c67c20feeecb336ff6bbf6e15c6fd3d4988c
xenial <farid.alijani@gmail.com>
2023-06-06 12:00:19 +0300

    elapsed time for preprocessed with examples split

--- tokenizer_utils.py
+++ tokenizer_utils.py
@@ -82,7 +82,7 @@ def stanza_lemmatizer(docs):
 	docs = re.sub(r'[%,+;,=&\'*"°^~?!—.•()“”:/‘’<>»«♦■\\\[\]-]+', ' ', docs ).strip()
 	
 	# print(f'preprocessed: len: {len(docs)}:\n{docs}')
-	print(f"{f'preprocessed doc contains { len( docs.split() ) } words':<30}{docs[:3]:<50}", end="")
+	print(f"{f'preprocessed doc contains { len( docs.split() ) } words':<30}{docs.split()[:3]:<50}", end="")
 	if ( not docs or len(docs)==0 ):
 		return
 
