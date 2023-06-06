082f3a83664870ef5d4acba1ad99371f60263763
xenial <farid.alijani@gmail.com>
2023-06-06 21:15:50 +0300

    narvi 86 sbatch no sep print match if else

--- utils.py
+++ utils.py
@@ -560,8 +560,9 @@ def get_df_pseudonymized_logs(infile="", TIMESTAMP=None):
 			# print (f">> matched line: {matched_line}")
 			if matched_line:
 				l = matched_line.groups()
-				patt = r'\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4}'
-				print(f"ts: {l[1]} => {l[1].replace(':', ' ', 1)} | {re.match(patt, l[1])}")
+				pattern = r'\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4}'
+				match = re.match(pattern, l[1])
+				print(f"ts: {l[1]} => {l[1].replace(':', ' ', 1)} | {f'YES' if match else 'NO'}")
 				#print("<>"*40)
 				cleaned_lines.append({
 					"user_ip": 							l[0],
