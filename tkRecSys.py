0e44ba95d464429fb402beb9237e92c51ea8f4ff
xenial <farid.alijani@gmail.com>
2023-06-08 19:03:51 +0300

    detail print endswith

--- tkRecSys.py
+++ tkRecSys.py
@@ -1205,8 +1205,8 @@ def plot_tokens_distribution(sparseMat, users_tokens_df, queryVec, recSysVec, bo
 def main():
 	try:
 		print(os.listdir(dfs_path))
-		print([f for f in os.listdir(dfs_path) if re.match(r'_concat.lz4', f)])
-		df_concat_fname = [f for f in os.listdir(dfs_path) if re.match(r"_concat.lz4", f)][0]
+		print( [ f for f in os.listdir(dfs_path) if f.endswith("_concat.lz4") ] )
+		df_concat_fname = [f for f in os.listdir(dfs_path) if f.endswith("_concat.lz4")][0]
 		df_raw = load_pickle(fpath=df_concat_fname)
 		print(df_raw.shape)
 		ndfs = int(df_concat_fname[:df_concat_fname.find("_")])
