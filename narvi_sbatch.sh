15d7dd5ba14371df975516ba7a53ba4d071bfecd
xenial <farid.alijani@gmail.com>
2023-06-12 01:35:03 +0300

    160-169 narvi

--- narvi_sbatch.sh
+++ narvi_sbatch.sh
@@ -8,8 +8,8 @@
 #SBATCH --partition=normal
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH --array=86
-# # # # # SBATCH --array=150-159
+# # # # # SBATCH --array=86
+#SBATCH --array=160-169
 
 stars=$(printf '%*s' 110 '')
 txt="SLURM JOB STARTED AT: `date`"
