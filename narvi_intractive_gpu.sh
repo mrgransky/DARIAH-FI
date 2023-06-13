5204ac9c18f07f91fb2ad2f6156cf25103410eff
xenial <farid.alijani@gmail.com>
2023-06-13 19:29:30 +0300

    narvi int gpu err fix

--- narvi_intractive_gpu.sh
+++ narvi_intractive_gpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-srun -J gpu_interactive  --partition=gpu --gres=gpu:teslap100:1 --time=02-23:59:59 --mem=48--pty /bin/bash -i
+srun -J gpu_interactive  --partition=gpu --gres=gpu:teslap100:1 --time=02-23:59:59 --mem=48G --pty /bin/bash -i
 #module load git
 #module load pytorch
