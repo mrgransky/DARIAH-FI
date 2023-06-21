aeb584031451270d88ce4e85261e0bde27c181f1
xenial <farid.alijani@gmail.com>
2023-06-21 11:59:21 +0300

    mem usage for all at once print included elps time

--- tkRecSys.py
+++ tkRecSys.py
@@ -1158,12 +1158,14 @@ def main():
 	print(f">> groupby...")
 	d = dict()
 	for n, g in usr_tk_dfs.groupby("user_ip"):
-		print(n)
+		st_t_f = time.time()
+		print(n, end=" ")
 		# print(g)
-		print()
-		print(g.loc[:, g.columns!="user_ip"].sum().values)
+		# print()
+		print(g.loc[:, g.columns!="user_ip"].sum().values, end=" ")
 		d[n] = g.loc[:, g.columns.difference(['user_ip'])].sum()
-		print("#"*100)
+		print(f"\tElapsed_t: {time.time()-st_t_f:.2f} s")
+		# print("#"*100)
 	user_token_df = pd.DataFrame.from_dict(d, orient="index")
 	print(f"\tElapsed_t: {time.time()-st_t:.2f} s")
 
