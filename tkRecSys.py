916204367515eff96c82b12ec7d6dfd5f64c1d60
xenial <farid.alijani@gmail.com>
2023-06-21 14:23:01 +0300

    gputest 15 min 256 check speed non zero print err fix

--- tkRecSys.py
+++ tkRecSys.py
@@ -1168,7 +1168,7 @@ def main():
 		# print(g)
 		# print()
 		print(g.loc[:, g.columns!="user_ip"].sum().values, end="\t")
-		print(f"|non_zeros|: { ( g.loc[:, g.columns!="user_ip"].sum() > 0.0).sum() }", end="\t")
+		print(f"|non_zeros|: { ( g.loc[:, g.columns!='user_ip'].sum() > 0.0).sum() }", end="\t")
 		# d[n] = g.loc[:, g.columns.difference(['user_ip'])].sum()
 		d[n] = g.loc[:, g.columns!="user_ip"].sum()
 		print(f"\tElapsed_t: {time.time()-st_t_f:.2f} s")
