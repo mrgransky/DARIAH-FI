7eb197f826fa41b61503053430213a6fe597e13e
xenial <farid.alijani@gmail.com>
2023-06-28 16:15:32 +0300

    tk distribution only qu ax define color label err fix

--- tkRecSys.py
+++ tkRecSys.py
@@ -701,7 +701,7 @@ def plot_tokens_distribution(sparseMat, users_tokens_df, queryVec, recSysVec, bo
 	f, ax = plt.subplots()	
 	sc1 = ax.scatter(	x=sparse_df.index, 
 										y=sparse_df.iloc[:, qu_indices], 
-										label=list(sparse_df.iloc[:, qu_indices].columns)#f"{[k for k, v in bow.items() if v==col]} | {col}",
+										label=list(sparse_df.iloc[:, qu_indices].columns),#f"{[k for k, v in bow.items() if v==col]} | {col}",
 										marker="H",
 										s=260,
 										facecolor="none", 
