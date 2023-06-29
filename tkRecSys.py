9c1e9490b3db760a6da3507b8e0ce09939ce943f
xenial <farid.alijani@gmail.com>
2023-06-29 14:55:08 +0300

    csv filename ch cosine show max print err

--- tkRecSys.py
+++ tkRecSys.py
@@ -260,10 +260,11 @@ def plot_cs(cos_sim, cos_sim_idx, QU, RF, query_phrase, query_token, users_token
 		N = np.count_nonzero(cos_sim.flatten())
 	
 	# nUsers_with_max_cosine = users_tokens_df.loc[cos_sim_idx.flatten()[:N], 'user_ip'].values
-	nUsers_with_max_cosine = users_tokens_df.loc[cos_sim_idx.flatten().max()[:N], 'user_ip'].values
-	print(nUsers_with_max_cosine)
 	print(cos_sim.flatten().max())
 	print(cos_sim_idx.flatten().max())
+
+	nUsers_with_max_cosine = users_tokens_df.loc[cos_sim_idx.flatten().max()[:N], 'user_ip'].values
+	print(nUsers_with_max_cosine)
 	
 	print(cos_sim.flatten().max()[:N])
 	print(cos_sim_idx.flatten().max()[:N])
