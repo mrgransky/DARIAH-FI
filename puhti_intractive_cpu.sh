6b4d46b412634dd736d1a9d7689d02d1a4c7f6c6
xenial <farid.alijani@gmail.com>
2023-06-30 15:44:58 +0300

    examine quvec column sorted cpu 350g large

--- puhti_intractive_cpu.sh
+++ puhti_intractive_cpu.sh
@@ -6,8 +6,8 @@
 # $ source puhti_intractive_cpu.sh
 
 # Image_Retrieval_TUNI
-srun -J intrtv_cpu --account=project_2004072 --partition=interactive --time=02-23:59:00 --mem=128G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
-# srun -J small_cpu --account=project_2004072 --partition=small --time=2-23:59:00 --mem=96G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+# srun -J intrtv_cpu --account=project_2004072 --partition=interactive --time=02-23:59:00 --mem=100G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J small_cpu --account=project_2004072 --partition=large --time=2-23:59:00 --mem=350G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 
 # APP_CSC
 #srun -J v100_intrc --account=Project_2004160 --partition=hugemem_longrun --time=5-23:59:00 --mem=512G --ntasks=1 --pty /bin/bash -i
