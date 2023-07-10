a5ac1d6fda463b86b0e958030b262edf3d6d3dd8
xenial <farid.alijani@gmail.com>
2023-07-10 21:24:30 +0300

    datasets_old in sh file # punc removal

--- tokenizer_utils.py
+++ tokenizer_utils.py
@@ -71,34 +71,32 @@ def spacy_tokenizer(sentence):
 
 def stanza_lemmatizer(docs):
 	# print(f'Raw: (len: {len(docs)}) >>{docs}<<')
-	print(f'Inp words: { len( docs.split() ) }', end=" ")
+	print(f"{f'Inp word(s): { len( docs.split() ) }':<60}", end="")
 	# st_t = time.time()
 	if not docs:
 		return
 	# treat all as document
 	docs = re.sub(r'\"|\'|<[^>]+>|[~*^][\d]+', '', docs)
-	docs = re.sub(r'[\{\}@®§©%,+;,=&\'€£*"°^~?!—.•()˶“”„:/|‘’<>»«□™♦_■\\\[\]-]+', ' ', docs ).strip()
+	docs = re.sub(r'[\{\}@®©§%,+;,=&\'€£#*"°^~?!—.•()˶“”„:/|‘’<>»«□™♦_■\\\[\]-]+', ' ', docs ).strip()
 	# docs = " ".join(map(str, [w for w in docs.split() if len(w)>2])) 
 	# docs = " ".join([w for w in docs.split() if len(w)>2])
 	docs = re.sub(r'\d+', "", docs)
 	docs = re.sub(r'\s{2,}', " ", re.sub(r'\b\w{,2}\b', ' ', docs).strip() ) # rm words with len() < 3 ex) ö v or l m and extra spaces 
 	
 	# print(f'preprocessed: len: {len(docs)}:\n{docs}')
-	print(f"{f'prepr. doc contains { len( docs.split() ) } words':<50}{str(docs.split()[:3]):<60}", end=" ")
+	print(f"{f'prepr. doc contain(s) { len( docs.split() ) } words':<60}{str(docs.split()[:3]):<80}", end="")
 	if not docs or len(docs)==0 or docs=="":
 		return
 	
 	st_t = time.time()
 	all_ = stanza_multi_pipeline(docs)
 	# print(all_.sentences)
-
-	# lm = [ re.sub('#|_','', word.lemma.lower()) for i, sent in enumerate(all_.sentences) for word in sent.words if ( word.lemma and len( re.sub(r'\b[A-Za-z](\.| |:)+', '', word.lemma ) ) > 2 and word.upos not in useless_upos_tags and word.lemma.lower() not in UNIQUE_STOPWORDS ) ]
 	lm = [ re.sub('#|_','', wlm.lower()) for i, sent in enumerate(all_.sentences) for word in sent.words if ( (wlm:=word.lemma) and len(wlm) > 2 and len( re.sub(r'\b[A-Za-z](\.| |:)+', '', wlm ) ) > 2 and word.upos not in useless_upos_tags and wlm.lower() not in UNIQUE_STOPWORDS ) ]
-	del docs, all_
-	gc.collect()
 	# print( lm )
 	print(f"Elapsed_t: {time.time()-st_t:.3f} s")
 	# print("<>"*70)
+	del docs, all_
+	gc.collect()
 	return lm
 
 def trankit_lemmatizer(docs):
