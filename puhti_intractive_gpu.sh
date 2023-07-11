159d6b4f2cc179005cc17cdc90a7c5dceafae18c
xenial <farid.alijani@gmail.com>
2023-07-11 11:07:08 +0300

    gpu 20g just srun

--- puhti_intractive_gpu.sh
+++ puhti_intractive_gpu.sh
@@ -8,7 +8,3 @@
 
 srun -J v100_gpu --account=project_2004072 --partition=gpu --gres=gpu:v100:1 --time=0-10:59:00 --mem-per-cpu=20G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 # srun -J v100_interactive --account=project_2004072 --partition=gputest --gres=gpu:v100:1 --time=0-00:15:00 --mem-per-cpu=16G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
-
-module load git
-#module load pytorch
-nvidia-smi
