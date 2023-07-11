9e16cf9df8ca0651a3f2f1fce047237047fae9ab
xenial <farid.alijani@gmail.com>
2023-07-11 22:02:34 +0300

    puhti sbathc

--- tokenizer_utils.py
+++ tokenizer_utils.py
@@ -37,10 +37,10 @@ with HiddenPrints():
 												},
 								}
 	stanza_multi_pipeline = MultilingualPipeline(	lang_id_config=lang_id_config, 
-																							use_gpu=True,
-																							lang_configs=lang_configs,
-																							download_method=DownloadMethod.REUSE_RESOURCES,
-																					)
+																								use_gpu=True,
+																								lang_configs=lang_configs,
+																								download_method=DownloadMethod.REUSE_RESOURCES,
+																							)
 
 STOPWORDS = nltk.corpus.stopwords.words(nltk.corpus.stopwords.fileids())
 my_custom_stopwords = ['btw', "could've", "n't","'s","—", "i'm", "'m", 
@@ -70,8 +70,8 @@ def spacy_tokenizer(sentence):
 	return lematized_tokens
 
 def stanza_lemmatizer(docs):
-	print(f'Raw: (len: {len(docs)})\n{docs}')
-	# print(f"{f'Inp word(s): { len( docs.split() ) }':<20}", end="")
+	# print(f'Raw: (len: {len(docs)})\n{docs}')
+	print(f"{f'Inp word(s): { len( docs.split() ) }':<20}", end="")
 	# st_t = time.time()
 	if not docs:
 		return
@@ -83,24 +83,28 @@ def stanza_lemmatizer(docs):
 	docs = re.sub(r'\d+', "", docs)
 	docs = re.sub(r'\s{2,}', " ", re.sub(r'\b\w{,2}\b', ' ', docs).strip() ) # rm words with len() < 3 ex) ö v or l m and extra spaces 
 	
-	print(f'preprocessed: len: {len(docs)}:\n{docs}')
-	# print(f"{f'prepr. doc contain(s) { len( docs.split() ) } words':<50}{str(docs.split()[:3]):<55}", end="")
+	# print(f'preprocessed: len: {len(docs)}:\n{docs}')
+	print(f"{f'prepr. with { len( docs.split() ) } words':<40}{str(docs.split()[:3]):<55}", end="")
+
 	if not docs or len(docs)==0 or docs=="":
 		return
 	
 	st_t = time.time()
 	all_ = stanza_multi_pipeline(docs)
-	for i, v in enumerate(all_.sentences):
-		print(i, v)
-		for iw, vw in enumerate(v.words):
-			print(iw, type(vw), vw.text, vw.upos, vw.lemma)
-			# assert vw.lemma.lower() == vw.lemma
-		print("#"*80)
-	print(f"{len(all_.sentences)} sent. with { [ len(sv.words) for _, sv in enumerate(all_.sentences) ] } words: { [ type(sv.words) for _, sv in enumerate(all_.sentences) ] }", end=" ")
-	lm = [ re.sub('#|_','', wlm) for _, sv in enumerate(all_.sentences) for w in sv.words if ( (wlm:=w.lemma.lower()) and len(wlm) > 2 and len( re.sub(r'\b[A-Za-z](\.| |:)+', '', wlm ) ) > 2 and w.upos not in useless_upos_tags and wlm.lower() not in UNIQUE_STOPWORDS ) ]
+
+	# for i, v in enumerate(all_.sentences):
+	# 	print(i, v)
+	# 	for iw, vw in enumerate(v.words):
+	# 		print(iw, type(vw), vw.text, vw.upos, vw.lemma)
+	# 	print("#"*80)
+
+	print(f"{f'{ len(all_.sentences) } sent. with { [ len(sv.words) for _, sv in enumerate(all_.sentences) ] } words':<50}", end="")
+
+	# lm = [ re.sub('#|_','', wlm) for _, sv in enumerate(all_.sentences) for w in sv.words if ( (wlm:=w.lemma.lower()) and len(wlm) > 2 and len( re.sub(r'\b[A-Za-z](\.| |:)+', '', wlm ) ) > 2 and w.upos not in useless_upos_tags and wlm not in UNIQUE_STOPWORDS ) ]
+	lm = [ re.sub('#|_','', wlm) for _, sv in enumerate(all_.sentences) for w in sv.words if ( (wlm:=w.lemma.lower()) and len(wlm) > 2 and w.upos not in useless_upos_tags and wlm not in UNIQUE_STOPWORDS ) ]
 	# print( lm )
 	print(f"Elapsed_t: {time.time()-st_t:.2f} s")
-	print("<>"*70)
+	# print("<>"*70)
 	del docs, all_
 	gc.collect()
 	return lm
