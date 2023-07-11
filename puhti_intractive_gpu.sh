d6f93a88d692bb4c0e432bbe20173adb36cdd5f6
xenial <farid.alijani@gmail.com>
2023-07-11 11:00:20 +0300

    gpu 20g

--- puhti_intractive_gpu.sh
+++ puhti_intractive_gpu.sh
@@ -6,13 +6,9 @@
 # $ source puhti_intractive_gpu.sh
 
 
-# Image_Retrieval_TUNI
-
-srun -J v100_32gb --account=project_2004072 --partition=gpu --gres=gpu:v100:1 --time=0-10:59:00 --mem-per-cpu=22G --ntasks=1 --cpus-per-task=1 nvidia-smi
+srun -J v100_gpu --account=project_2004072 --partition=gpu --gres=gpu:v100:1 --time=0-10:59:00 --mem-per-cpu=20G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 # srun -J v100_interactive --account=project_2004072 --partition=gputest --gres=gpu:v100:1 --time=0-00:15:00 --mem-per-cpu=16G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 
-# APP_CSC
-#srun -J v100_intrc --account=Project_2004160 --partition=gpu --gres=gpu:v100:1 --time=1-23:59:00 --mem=128G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
-
-#module load git
+module load git
 #module load pytorch
+nvidia-smi
