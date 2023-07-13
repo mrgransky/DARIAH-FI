910637a29b76294b9374409e985d26ca39810f68
xenial <farid.alijani@gmail.com>
2023-07-13 12:46:36 +0300

    raw and preprocessed only nkQQ0-1

--- puhti_sbatch_RecSys.sh
+++ puhti_sbatch_RecSys.sh
@@ -9,8 +9,8 @@
 #SBATCH --mem=256G
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
-# # # # # SBATCH --array=0-1
-#SBATCH --array=1
+#SBATCH --array=0-1
+# # # # # # SBATCH --array=1
 
 stars=$(printf '%*s' 90 '')
 txt="SLURM JOB STARTED AT: `date`"
