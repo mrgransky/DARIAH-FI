c0ce78eb201bf3b3ec7bf2d3258e09f054ef57fe
xenial <farid.alijani@gmail.com>
2023-07-14 17:26:14 +0300

    docs and cleaned docs to stanza print correct remove wrong

--- nlp_utils.py
+++ nlp_utils.py
@@ -140,7 +140,6 @@ def get_cBoWs(dframe: pd.DataFrame, fprefix: str="df_concat", lm: str="stanza"):
 	# except Exception as e:
 	# 	logging.exception(e)
 	except:
-		# print(f"Training TFIDF vector for {len(raw_docs_list)} raw words/phrases/sentences, might take a while...".center(150, " "))
 		print(f"Training TFIDF vector for {len(preprocessed_docs)} raw words/phrases/sentences, might take a while...".center(150, " "))
 		st_t = time.time()
 		# Initialize TFIDF # not time consuming...
