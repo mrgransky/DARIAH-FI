1b793bd17dccdafcbbe4664ca28520bb801d909a
xenial <farid.alijani@gmail.com>
2023-08-08 13:28:18 +0300

    snippet last row using values -1

--- nlp_utils.py
+++ nlp_utils.py
@@ -98,10 +98,8 @@ def get_cBoWs(dframe: pd.DataFrame, fprefix: str="df_concat", lm: str="stanza"):
 	print("#"*100)
 	print(dframe['snippet_raw_text'].tail(10))
 	print("#"*100)
-	print( type( dframe['snippet_raw_text'].tail(1).values ), len( dframe['snippet_raw_text'].tail(1).values ) )
-	print(dframe['snippet_raw_text'].tail(1).values)
-	print("."*100)
-	print(dframe['snippet_raw_text'].tail(1).values.tolist())
+	print( type( dframe['snippet_raw_text'].values[-1:] ), len( dframe['snippet_raw_text'].values[-1:] ) )
+	print(dframe['snippet_raw_text'].values[-1:])
 	sys.exit()
 
 	users_list = list()
