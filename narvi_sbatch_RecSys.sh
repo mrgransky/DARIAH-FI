3ad8405ad79f4e2b2c12353a82cac9c034a5b1dd
xenial <farid.alijani@gmail.com>
2023-08-15 10:52:45 +0300

    nikeXY check

--- narvi_sbatch_RecSys.sh
+++ narvi_sbatch_RecSys.sh
@@ -8,7 +8,7 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:teslav100:1
 #SBATCH --mem=13G
-#SBATCH --array=0-1
+#SBATCH --array=702-703
 
 stars=$(printf '%*s' 90 '')
 txt="SLURM JOB STARTED AT: `date`"
