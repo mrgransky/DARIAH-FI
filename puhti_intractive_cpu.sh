afa3234bc7a9ab3c7d5d1e35c6b2e1bf83461672
xenial <farid.alijani@gmail.com>
2023-08-24 11:13:26 +0300

    decrease puhti interactive partition

--- puhti_intractive_cpu.sh
+++ puhti_intractive_cpu.sh
@@ -6,8 +6,8 @@
 # $ source puhti_intractive_cpu.sh
 
 # Image_Retrieval_TUNI
-# srun -J intrtv_cpu --account=project_2004072 --partition=interactive --time=02-23:59:00 --mem=64G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
-srun -J intrtv_cpu --account=project_2004072 --partition=small --time=00-23:59:00 --mem=32G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J intrtv_cpu --account=project_2004072 --partition=interactive --time=02-23:59:00 --mem=64G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+# srun -J intrtv_cpu --account=project_2004072 --partition=small --time=00-23:59:00 --mem=32G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 # srun -J small_cpu --account=project_2004072 --partition=large --time=2-23:59:00 --mem=128G --ntasks=1 --cpus-per-task=2 --pty /bin/bash -i
 
 module load git
