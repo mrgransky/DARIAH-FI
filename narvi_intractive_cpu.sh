ee7f1264f930595b0e53d70f898e6ef15c0ce0b5
xenial <farid.alijani@gmail.com>
2023-08-28 21:48:08 +0300

    time for del and gc interactive normal

--- narvi_intractive_cpu.sh
+++ narvi_intractive_cpu.sh
@@ -5,4 +5,4 @@
 # OR ...
 # $ source puhti_intractive_cpu.sh
 
-srun -J cpu_intractive  --partition=small --time=00-23:59:59 --mem=256G --pty /bin/bash -i
+srun -J cpu_intractive  --partition=normal --time=00-23:59:59 --mem=256G --pty /bin/bash -i
