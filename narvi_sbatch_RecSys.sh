9ffaf95c36d94b43593e3018d8d2bdb8535be643
xenial <farid.alijani@gmail.com>
2023-08-29 17:30:39 +0300

    730-731 with normal partition 512g mem check

--- narvi_sbatch_RecSys.sh
+++ narvi_sbatch_RecSys.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
 #SBATCH --time=01-00:00:00
-#SBATCH --partition=grid
+#SBATCH --partition=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
