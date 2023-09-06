7f500296b848ef4bf3a4a9344e8fc509ce918797
xenial <farid.alijani@gmail.com>
2023-09-07 00:04:30 +0300

    df_concat_XY puhti hugemem 780g

--- puhti_sbatch_RecSys.sh
+++ puhti_sbatch_RecSys.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=980G
+#SBATCH --mem=780G
 #SBATCH --partition=hugemem
 #SBATCH --time=07-00:00:00
 # # # # # # SBATCH --gres=gpu:v100:1
