cebd14acd6f98c802f8701f4df4b0b40c39219d9
xenial <farid.alijani@gmail.com>
2023-09-13 21:33:21 +0300

    renames saving files fprefix try except name fixed no elem for read_parquet narvi grid partition

--- narvi_sbatch_RecSys.sh
+++ narvi_sbatch_RecSys.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=264G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=normal
+#SBATCH --partition=grid
 # # # # SBATCH --gres=gpu:rtx100:1
 # # # # SBATCH --nodes=1
 # # # # SBATCH --array=0-731
