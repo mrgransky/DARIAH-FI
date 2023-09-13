1fc81eb343eb0d6b7ab605113ac83202953ef294
xenial <farid.alijani@gmail.com>
2023-09-13 20:34:03 +0300

    XXX nUSRs save list of dfs

--- tkRecSys.py
+++ tkRecSys.py
@@ -748,6 +748,9 @@ def get_users_tokens_df():
 		users_tokens_dfs.append(user_token_df)
 	print(f"Loaded {len(users_tokens_dfs)} users_tokens_dfs in {time.time()-load_time_start:.1f} sec".center(180, "-"))
 	gc.collect()
+	
+	usr_tk_pdfs_list_fname = os.path.join(args.dsPath, f"{fprefix}_lemmaMethod_{args.lmMethod}_{len(users_tokens_dfs)}_usr_tk_pdfs_list.gz")
+	save_pickle(pkl=users_tokens_dfs, fname=usr_tk_pdfs_list_fname)
 
 	print(f"[PANDAS] chain concatination of {len(users_tokens_dfs)} user_token_dfs", end="\t")
 	st_t = time.time()
@@ -811,6 +814,9 @@ def get_users_tokens_ddf():
 		users_tokens_dfs.append(user_token_df)
 	print(f"Loaded {len(users_tokens_dfs)} users_tokens_dfs in {time.time()-load_time_start:.1f} s".center(180, "-"))
 	gc.collect()
+
+	usr_tk_pdfs_list_fname = os.path.join(args.dsPath, f"{fprefix}_lemmaMethod_{args.lmMethod}_{len(users_tokens_dfs)}_usr_tk_pdfs_list.gz")
+	save_pickle(pkl=users_tokens_dfs, fname=usr_tk_pdfs_list_fname)
 
 	print(f"[DASK] Concatinating {len(users_tokens_dfs)} users_tokens_dfs & GroupBy user_ip column", end="\t")
 	st_t = time.time()
