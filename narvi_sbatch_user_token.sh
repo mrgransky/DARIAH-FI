dbc9b557e34fc73a695e799f154b835d20ddc772
xenial <farid.alijani@gmail.com>
2023-09-27 11:31:29 +0300

    explore max_df min_df using for loop narvi mx mn adjusted rtx and xy adjusted

--- narvi_sbatch_user_token.sh
+++ narvi_sbatch_user_token.sh
@@ -9,10 +9,10 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:teslav100:1
+#SBATCH --gres=gpu:rtx100:1
 #SBATCH --nodes=1
-#SBATCH --array=730-731
-
+# # # # SBATCH --array=730-731 # originall case! xy
+#SBATCH --array=628-629
 user="`whoami`"
 stars=$(printf '%*s' 100 '')
 txt="$user began Slurm job: `date`"
