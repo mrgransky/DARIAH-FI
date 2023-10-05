6560658b57ffb8dfd6d86fb2afb35fe1a275689a
xenial <farid.alijani@gmail.com>
2023-10-05 19:42:33 +0300

    puhti 1396g

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=996G
+#SBATCH --mem=1396G
 #SBATCH --partition=hugemem_longrun
 #SBATCH --time=01-00:00:00
 #SBATCH --nodes=1
