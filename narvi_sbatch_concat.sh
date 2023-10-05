b01344d42c7e06503b251b0671e68e3ef743cb83
xenial <farid.alijani@gmail.com>
2023-10-05 13:12:16 +0300

    narvi check concat gc collect 7070g

--- narvi_sbatch_concat.sh
+++ narvi_sbatch_concat.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
 #SBATCH --time=07-00:00:00
-#SBATCH --mem=700G
+#SBATCH --mem=770G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=normal
