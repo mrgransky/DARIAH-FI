eccd70aba566ff439aa3e34419ee60569d63e9a5
Farid Alijani <farid.alijani@gmail.com>
2023-10-15 11:34:26 +0300

    200 gpu continue

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --account=project_2004072
-#SBATCH --job-name=df_concat
+#SBATCH --job-name=df_concat_continue
 #SBATCH --output=/scratch/project_2004072/Nationalbiblioteket/trash/NLF_logs/%x_%N_%j.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
