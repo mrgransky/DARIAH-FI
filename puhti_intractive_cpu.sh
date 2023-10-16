34dc836365bb8d035d1682339b45b0f4aa5cdbe4
Farid Alijani <farid.alijani@gmail.com>
2023-10-16 15:01:37 +0300

    dfs spm interactive tr

--- puhti_intractive_cpu.sh
+++ puhti_intractive_cpu.sh
@@ -6,8 +6,8 @@
 # $ source puhti_intractive_cpu.sh
 
 # Image_Retrieval_TUNI
-# srun -J intrtv_cpu --account=project_2004072 --partition=interactive --time=07-00:00:00 --mem=168G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
-srun -J intrtv_cpu --account=project_2004072 --partition=small --time=03-00:00:00 --mem=192G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J intrtv_cpu --account=project_2004072 --partition=interactive --time=07-00:00:00 --mem=168G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+# srun -J intrtv_cpu --account=project_2004072 --partition=small --time=03-00:00:00 --mem=192G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 # srun -J cpu_large_interactive --account=project_2004072 --partition=large --time=00-23:59:00 --mem=286G --ntasks=1 --cpus-per-task=2 --pty /bin/bash -i
 
 module load git
