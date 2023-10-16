83cea7a709f73ebdab8e57a5fe33b8134d445dac
Farid Alijani <farid.alijani@gmail.com>
2023-10-16 13:24:11 +0300

    dfs spm in list

--- concat_dfs.py
+++ concat_dfs.py
@@ -719,7 +719,7 @@ def get_users_tokens_df():
 			print(f"< Sanity Check > {len(zero_cols)} column(s) of ALL zeros: {zero_cols} | Elapsed_t: {time.time()-st_t:.3f} sec".center(150, " "))
 			assert len(zero_cols)==0, f"<!> Error! There exist {len(zero_cols)} column(s) with all zero values!"
 			# users_tokens_dfs.append(user_token_df) # original PANDAS df
-			users_tokens_dfs.append(get_df_spm(user_token_df)) # to SparseDtype
+			users_tokens_dfs.append(get_df_spm(df=user_token_df)) # to SparseDtype
 			
 		print(f"Loaded {len(users_tokens_dfs)} users_tokens_dfs in {time.time()-load_time_start:.1f} sec".center(180, "-"))
 		gc.collect()
