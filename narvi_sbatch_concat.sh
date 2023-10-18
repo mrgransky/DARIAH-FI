1381c143697eb3aea3938fcdd4e6b602b48d2008
Farid Alijani <farid.alijani@gmail.com>
2023-10-19 00:29:57 +0300

    get_unpacked narvi amdgpu check

--- narvi_sbatch_concat.sh
+++ narvi_sbatch_concat.sh
@@ -8,9 +8,9 @@
 #SBATCH --mem=128G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:teslap100:1
+#SBATCH --partition=amdgpu
 #SBATCH --nodes=1
+# # # # # # SBATCH --gres=gpu:teslap100:1
 
 user="`whoami`"
 stars=$(printf '%*s' 100 '')
