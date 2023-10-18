65a2caa1b167b23b326c288abc520c5fb73f41ae
Farid Alijani <farid.alijani@gmail.com>
2023-10-19 01:08:18 +0300

    get_unpacked puhti small

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=128G
+#SBATCH --mem=300G
 #SBATCH --partition=small
 #SBATCH --time=3-00:00:00
 #SBATCH --nodes=1
