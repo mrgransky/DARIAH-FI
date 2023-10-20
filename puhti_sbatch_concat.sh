6a082023869925946a50af78cb97e5993fcd2dd3
Farid Alijani <farid.alijani@gmail.com>
2023-10-20 15:36:50 +0300

    puhti x14 hugemem 1 40 10 10hr

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
-#SBATCH --mem-per-cpu=10G
-#SBATCH --partition=large
+#SBATCH --mem-per-cpu=25G
+#SBATCH --partition=hugemem
 #SBATCH --time=00-10:00:00
 #SBATCH --nodes=1
 ######SBATCH --gres=gpu:v100:1
