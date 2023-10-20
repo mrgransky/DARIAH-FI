1bc01e131e519ba30fd2007c03355b4153e0ce9b
Farid Alijani <farid.alijani@gmail.com>
2023-10-20 15:27:40 +0300

    puhti x14 hugmem 1 40 10

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -7,9 +7,9 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
-#SBATCH --mem-per-cpu=25G
+#SBATCH --mem-per-cpu=10G
 #SBATCH --partition=hugemem
-#SBATCH --time=00-20:00:00
+#SBATCH --time=00-10:00:00
 #SBATCH --nodes=1
 ######SBATCH --gres=gpu:v100:1
 
