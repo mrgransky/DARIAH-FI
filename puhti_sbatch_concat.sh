10953bc8ea9c76af497eabd849b7a6c92e98f328
Farid Alijani <farid.alijani@gmail.com>
2023-10-22 19:47:59 +0300

    check 40*37 puhti

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
-#SBATCH --mem-per-cpu=38G
+#SBATCH --mem-per-cpu=37G
 #SBATCH --partition=hugemem
 #SBATCH --time=00-10:00:00
 #SBATCH --nodes=1
