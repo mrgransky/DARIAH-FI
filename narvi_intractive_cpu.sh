bb9da6f28380d493022e7b63745e74f44c15c3f2
Farid Alijani <farid.alijani@gmail.com>
2023-10-24 20:57:31 +0300

    concat spms for intractive 32g

--- narvi_intractive_cpu.sh
+++ narvi_intractive_cpu.sh
@@ -5,4 +5,4 @@
 # OR ...
 # $ source puhti_intractive_cpu.sh
 
-srun -J cpu_intractive  --partition=normal --time=06-23:59:59 --mem=700G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J cpu_intractive  --partition=normal --time=00-23:59:59 --mem=32G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
