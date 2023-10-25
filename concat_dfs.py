41a17a278dc1138184e2dba8c0c4de4e2960ba52
Farid Alijani <farid.alijani@gmail.com>
2023-10-25 16:01:15 +0300

    fnames modified check print no try except

--- concat_dfs.py
+++ concat_dfs.py
@@ -829,22 +829,27 @@ def run():
 	fprefix = f"concatinated_{len(get_spm_files(fpath=args.dfsPath+'/'+'*_USERs_TOKENs_spm_U_x_T_*_BoWs.gz' ))}_SPMs"
 	RES_DIR = make_result_dir(infile=fprefix)
 	print(fprefix, RES_DIR)
-	print(args.dfsPath+'/'+f'{fprefix}_*_USERs_TOKENs_spm_U_x_T_*_BoWs.gz')
-	print(args.dfsPath+'/'+f'{fprefix}_*_USERs_TOKENs_spm_user_ip_names_*_BoWs.gz')
-	print(args.dfsPath+'/'+f'{fprefix}_*_USERs_TOKENs_spm_token_names_*_BoWs.gz')
-	print(f"<>"*100)
-	
-	try:
-		# load
-		usr_tk_spm = load_pickle(fpath=os.path.join(args.dfsPath, f"{fprefix}_lemmaMethod_{args.lmMethod}_USERs_TOKENs_spm_U_x_T_{len(BoWs)}_BoWs.gz"))
-		usr_tk_spm_usrNames = load_pickle(fpath=os.path.join(args.dfsPath, f"{fprefix}_lemmaMethod_{args.lmMethod}_USERs_TOKENs_spm_user_ip_names_{len(BoWs)}_BoWs.gz"))
-		usr_tk_spm_tokNames = load_pickle(fpath=os.path.join(args.dfsPath, f"{fprefix}_lemmaMethod_{args.lmMethod}_USERs_TOKENs_spm_token_names_{len(BoWs)}_BoWs.gz"))
-	except Exception as e:
-		print(f"<!> {e}")
-		usr_tk_spm, usr_tk_spm_usrNames, usr_tk_spm_tokNames=get_sparse_user_token(	spm_fname=args.dfsPath+'/'+f'{fprefix}_*_USERs_TOKENs_spm_U_x_T_*_BoWs.gz',
-																																								spm_rows_fname=args.dfsPath+'/'+'*_USERs_TOKENs_spm_user_ip_names_*_BoWs.gz',
-																																								spm_cols_fname=args.dfsPath+'/'+'*_USERs_TOKENs_spm_token_names_*_BoWs.gz' ,
-																																							)
+	# print(args.dfsPath+'/'+f'{fprefix}_*_USERs_TOKENs_spm_U_x_T_*_BoWs.gz')
+	# print(args.dfsPath+'/'+f'{fprefix}_*_USERs_TOKENs_spm_user_ip_names_*_BoWs.gz')
+	# print(args.dfsPath+'/'+f'{fprefix}_*_USERs_TOKENs_spm_token_names_*_BoWs.gz')
+	# print(f"<>"*100)
+
+	# try:
+	# 	# load
+	# 	usr_tk_spm = load_pickle(fpath=os.path.join(args.dfsPath, f"{fprefix}_lemmaMethod_{args.lmMethod}_USERs_TOKENs_spm_U_x_T_{len(BoWs)}_BoWs.gz"))
+	# 	usr_tk_spm_usrNames = load_pickle(fpath=os.path.join(args.dfsPath, f"{fprefix}_lemmaMethod_{args.lmMethod}_USERs_TOKENs_spm_user_ip_names_{len(BoWs)}_BoWs.gz"))
+	# 	usr_tk_spm_tokNames = load_pickle(fpath=os.path.join(args.dfsPath, f"{fprefix}_lemmaMethod_{args.lmMethod}_USERs_TOKENs_spm_token_names_{len(BoWs)}_BoWs.gz"))
+	# except Exception as e:
+	# 	print(f"<!> {e}")
+	# 	usr_tk_spm, usr_tk_spm_usrNames, usr_tk_spm_tokNames=get_sparse_user_token(	spm_fname=args.dfsPath+'/'+f'{fprefix}_*_USERs_TOKENs_spm_U_x_T_*_BoWs.gz',
+	# 																																							spm_rows_fname=args.dfsPath+'/'+'*_USERs_TOKENs_spm_user_ip_names_*_BoWs.gz',
+	# 																																							spm_cols_fname=args.dfsPath+'/'+'*_USERs_TOKENs_spm_token_names_*_BoWs.gz' ,
+	# 																																						)
+
+	usr_tk_spm, usr_tk_spm_usrNames, usr_tk_spm_tokNames=get_sparse_user_token(	spm_fname=args.dfsPath+'/'+f'{fprefix}_*_USERs_TOKENs_spm_U_x_T_*_BoWs.gz',
+																																							spm_rows_fname=args.dfsPath+'/'+'*_USERs_TOKENs_spm_user_ip_names_*_BoWs.gz',
+																																							spm_cols_fname=args.dfsPath+'/'+'*_USERs_TOKENs_spm_token_names_*_BoWs.gz' ,
+																																						)
 
 def main():
 	print(f"Running {__file__} with {args.lmMethod.upper()} lemmatizer ...")
