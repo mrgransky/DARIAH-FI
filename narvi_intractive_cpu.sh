d23acc06195b2001c1761a83ee43375af3a61f7a
Farid Alijani <farid.alijani@gmail.com>
2023-10-25 18:15:04 +0300

    nikeXY puhti saving spm check err intractive cpu narvi

--- narvi_intractive_cpu.sh
+++ narvi_intractive_cpu.sh
@@ -5,4 +5,4 @@
 # OR ...
 # $ source puhti_intractive_cpu.sh
 
-srun -J cpu_intractive  --partition=amd --time=00-23:59:59 --mem=128G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J cpu_intractive  --partition=amd --time=00-03:59:59 --mem=256G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
