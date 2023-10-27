f0d8242ad7bc75ceaf7a09bdfabf578ef755a609
Farid Alijani <farid.alijani@gmail.com>
2023-10-27 20:30:54 +0300

    interactive for cleaner print interactive 16g

--- puhti_intractive_cpu.sh
+++ puhti_intractive_cpu.sh
@@ -7,7 +7,7 @@
 
 # Image_Retrieval_TUNI
 # srun -J intrtv_cpu --account=project_2004072 --partition=hugemem --time=00-05:15:00 --mem=999G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
-srun -J intrtv_cpu --account=project_2004072 --partition=small --time=00-01:00:00 --mem=16G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J intrtv_cpu --account=project_2004072 --partition=interactive --time=00-01:00:00 --mem=16G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 # srun -J cpu_large_interactive --account=project_2004072 --partition=large --time=00-23:59:00 --mem=286G --ntasks=1 --cpus-per-task=2 --pty /bin/bash -i
 
 module load git
