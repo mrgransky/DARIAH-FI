f9ca0f08f4096e6a0a601dcff55d71d1d61915bc
Farid Alijani <farid.alijani@gmail.com>
2023-10-31 18:28:06 +0200

    puhti sbatch x251 check efficiency rearrange queries dataframe x251 999g

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=889G
+#SBATCH --mem-per-cpu=999G
 #SBATCH --partition=hugemem_longrun
 #SBATCH --time=14-00:00:00
 ######SBATCH --gres=gpu:v100:1
