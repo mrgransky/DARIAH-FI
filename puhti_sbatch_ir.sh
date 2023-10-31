dd3642bc458ced1ce8f8cc56348715581d68636b
Farid Alijani <farid.alijani@gmail.com>
2023-11-01 00:21:10 +0200

    puhti irQ 910 1100

--- puhti_sbatch_ir.sh
+++ puhti_sbatch_ir.sh
@@ -11,7 +11,7 @@
 #SBATCH --time=03-00:00:00
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
-#SBATCH --array=899-1100
+#SBATCH --array=910-1100
 #########SBATCH --array=641-890%10
 
 stars=$(printf '%*s' 100 '')
