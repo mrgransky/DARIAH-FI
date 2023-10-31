dc3266ec5a1a73fea3683b48185ea7a0d535570b
Farid Alijani <farid.alijani@gmail.com>
2023-10-31 15:54:57 +0200

    puhti hugemem_longrun xy sort_idx

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -7,10 +7,10 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=100G
-#SBATCH --partition=large
-#SBATCH --time=03-00:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=489G
+#SBATCH --partition=hugemem
+#SBATCH --time=00-18:00:00
 ######SBATCH --gres=gpu:v100:1
 
 user="`whoami`"
