c7c5e046bbed6dc4c97b800105a3e22c778e0a1b
Farid Alijani <farid.alijani@gmail.com>
2023-11-01 01:21:04 +0200

    puhti user_token 501-750

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -7,12 +7,12 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=165G
+#SBATCH --mem=155G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:v100:1
-#SBATCH --array=251-500
+#SBATCH --array=501-750
 
 user="`whoami`"
 stars=$(printf '%*s' 100 '')
