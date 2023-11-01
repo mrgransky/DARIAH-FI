8b062476b013f04f3604daef244c010f214d89e2
Farid Alijani <farid.alijani@gmail.com>
2023-11-01 12:35:08 +0200

    puhti interactive small 128g

--- puhti_intractive_cpu.sh
+++ puhti_intractive_cpu.sh
@@ -7,7 +7,7 @@
 
 # Image_Retrieval_TUNI
 # srun -J intrtv_cpu --account=project_2004072 --partition=test --time=00-00:15:00 --mem=16G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
-srun -J intrtv_cpu --account=project_2004072 --partition=small --time=00-23:00:00 --mem=32G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J intrtv_cpu --account=project_2004072 --partition=small --time=00-23:00:00 --mem=128G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 # srun -J cpu_large_interactive --account=project_2004072 --partition=large --time=00-23:59:00 --mem=64G --ntasks=1 --cpus-per-task=2 --pty /bin/bash -i
 
 module load git
