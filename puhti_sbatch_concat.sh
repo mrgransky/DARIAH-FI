4f3f20e282b595012b910529e659b9231482459b
Farid Alijani <farid.alijani@gmail.com>
2023-11-01 18:36:59 +0200

    puhti sbatch with 8 x 199g

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=399G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=199G
 #SBATCH --partition=hugemem_longrun
 #SBATCH --time=14-00:00:00
 ######SBATCH --gres=gpu:v100:1
