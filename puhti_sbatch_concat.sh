cb044734fa299c97269add3336acee4103e37339
Farid Alijani <farid.alijani@gmail.com>
2023-11-01 20:06:08 +0200

    puhti sbatch with 30*49g nogil=true

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=30
-#SBATCH --mem-per-cpu=50G
+#SBATCH --mem-per-cpu=49G
 #SBATCH --partition=hugemem_longrun
 #SBATCH --time=14-00:00:00
 ######SBATCH --gres=gpu:v100:1
