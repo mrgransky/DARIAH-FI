2d3c61e0737a6c467345317e6a9a2f9f4139f29b
Farid Alijani <farid.alijani@gmail.com>
2023-11-01 18:26:59 +0200

    puhti interactive small fastmath numba jit assign 40 cores

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1099G
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=399G
 #SBATCH --partition=hugemem_longrun
 #SBATCH --time=14-00:00:00
 ######SBATCH --gres=gpu:v100:1
