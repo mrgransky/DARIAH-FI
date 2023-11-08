a947d47ef7c94d14150b17f70c34863ff0af0502
Farid Alijani <farid.alijani@gmail.com>
2023-11-09 00:53:36 +0200

    concat x251 new res

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,9 +8,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=780G
-#SBATCH --partition=hugemem_longrun
-#SBATCH --time=14-00:00:00
+#SBATCH --mem=256G
+#SBATCH --partition=small
+#SBATCH --time=13-00:00:00
 ######SBATCH --mem-per-cpu=50G
 
 user="`whoami`"
