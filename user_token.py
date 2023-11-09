04ba9642557468061186315c1a5bca8fdf701843
Farid Alijani <farid.alijani@gmail.com>
2023-11-09 12:50:26 +0200

    check with args.maxNumFeatures no assign def

--- user_token.py
+++ user_token.py
@@ -24,7 +24,7 @@ parser.add_argument('--qphrase', default="Juha Sipilä Sahalahti", type=str)
 parser.add_argument('--lmMethod', default="stanza", type=str)
 parser.add_argument('--normSP', default=False, type=bool)
 parser.add_argument('--topTKs', default=5, type=int)
-parser.add_argument('--maxNumFeat', default=None) # default: None => all retrieved tokens extracted!
+parser.add_argument('--maxNumFeat') # default: None => all retrieved tokens extracted!
 parser.add_argument('-maxdf', '--minDocFreq', default=1, type=int)
 parser.add_argument('-mindf', '--maxDocFreq', default=1.0, type=float)
 
@@ -516,6 +516,8 @@ def main():
 	if args.maxNumFeat is not None:
 		print("HEEEEREEEEE.....")
 		args.outDIR+=f"_maxNumFeatures_{args.maxNumFeat}"
+	else:
+		print("elseeeeeeeeeeeeeeeeeee...........")
 	make_folder(folder_name=args.outDIR)
 	return
 	try:
