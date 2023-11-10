a0cad80ef1356476da6a3ceeb1032f63ebfa9521
Farid Alijani <farid.alijani@gmail.com>
2023-11-10 20:20:35 +0200

    puhti x732 large 373g

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=375G
+#SBATCH --mem=373G
 #SBATCH --partition=large 
 #SBATCH --time=03-00:00:00
 ######SBATCH --mem-per-cpu=50G
