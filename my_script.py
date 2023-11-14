2419b0d13df3619f703b8279fe9af59b90736db1
Farid Alijani <farid.alijani@gmail.com>
2023-11-14 14:19:10 +0200

    costum_stop_words read from file

--- my_script.py
+++ my_script.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--my_arg', default=None, type=int)
+parser.add_argument('--my_arg', default=None, type=int, required=True)
 parser.add_argument('--second_arg_name', default="Sample text", type=str)
 args = parser.parse_args()
 
@@ -13,3 +13,12 @@ if args.my_arg:
 	print('my_arg argument is:', args.my_arg)
 else:
 	print('my_arg argument was not specified')
+
+# Open the file in read mode
+with open('my_file.txt', 'r') as file:
+	# Read lines from the file and remove trailing newline characters
+	words = [line.strip() for line in file]
+
+# Now 'words' is a list containing the words from your file
+print(words)
+print(len(words))
