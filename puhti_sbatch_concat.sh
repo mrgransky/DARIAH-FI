f6f05c12c115d87f64af8c332e5ca582e04f7fc9
Farid Alijani <farid.alijani@gmail.com>
2023-11-14 20:30:42 +0200

    puhti concat hugemem longrun 573g 14days

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -10,7 +10,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=573G
 #SBATCH --partition=hugemem_longrun 
-#SBATCH --time=04-00:00:00
+#SBATCH --time=14-00:00:00
 ######SBATCH --mem-per-cpu=50G
 
 user="`whoami`"
