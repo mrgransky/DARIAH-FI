03b7f1606c375219c646e704f995681f3a14397e
Farid Alijani <farid.alijani@gmail.com>
2023-11-17 18:29:13 +0200

    puhti 504-732 gpu 192g

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #SBATCH --account=project_2004072
-#SBATCH --job-name=nikeXY
+#SBATCH --job-name=nikeQ
 #SBATCH --output=/scratch/project_2004072/Nationalbiblioteket/trash/NLF_logs/%x_%a_%N_%j_%A.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=256G
+#SBATCH --mem=192G
 #SBATCH --partition=gpu
-#SBATCH --time=00-04:00:00
+#SBATCH --time=03-00:00:00
 #SBATCH --gres=gpu:v100:1
-#SBATCH --array=731
+#SBATCH --array=504-732
 
 user="`whoami`"
 stars=$(printf '%*s' 100 '')
@@ -33,7 +33,7 @@ echo "${stars// /*}"
 
 echo "$SLURM_CLUSTER_NAME conda env from tykky module..."
 files=(/scratch/project_2004072/Nationalbiblioteket/datasets/*.dump)
-ddir="/scratch/project_2004072/Nationalbiblioteket/dataframes_XY"
+ddir="/scratch/project_2004072/Nationalbiblioteket/dataframes"
 # maxNumFeatures=$(awk -v x="1.9e+6" 'BEGIN {printf("%d\n",x)}') # adjust values 2.2e+6
 maxNumFeatures=-1
 
