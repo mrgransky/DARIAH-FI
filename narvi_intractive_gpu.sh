abf152ffe74420f9ea4a765abd9edf042beb87df
Farid Alijani <farid.alijani@gmail.com>
2023-11-22 13:58:04 +0200

    gpu test v100 narvi

--- narvi_intractive_gpu.sh
+++ narvi_intractive_gpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-srun -J gpu_interactive  --partition=gpu --gres=gpu:rtx100:1 --time=00-03:59:59 --mem=8G --pty /bin/bash -i
+srun -J gpu_interactive  --partition=test --gres=gpu:teslav100:1 --time=00-04:00:00 --mem=128G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 #module load git
 #module load pytorch
