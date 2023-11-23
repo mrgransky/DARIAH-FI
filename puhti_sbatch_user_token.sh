43ca0c38ccf84c43583f3646ab914475f65ade14
Farid Alijani <farid.alijani@gmail.com>
2023-11-23 23:47:35 +0200

    367-369 hugemem_longrun partition 399g 1cpus/tasks

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -7,9 +7,9 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=373G
-#SBATCH --partition=large
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=399G
+#SBATCH --partition=hugemem_longrun
 #SBATCH --time=00-23:59:00
 #SBATCH --array=367-369
 ####SBATCH --gres=gpu:v100:1
