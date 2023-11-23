5c6cd7fbf01dd7bc1326c48403d3ed33cbf094b3
Farid Alijani <farid.alijani@gmail.com>
2023-11-23 12:01:15 +0200

    367-560 256g gpu

--- gui.py
+++ gui.py
@@ -1,7 +1,11 @@
 import tkinter as tk
-import webbrowser
+from tkinter import messagebox 
 from urllib.parse import quote
+from PIL import Image, ImageTk
+import webbrowser
 import time
+import requests
+from io import BytesIO
 
 def on_entry_click(event):
 	query = entry.get()
@@ -25,6 +29,7 @@ def generate_link():
 		nlf_link_lable.bind("<Button-1>", lambda e: webbrowser.open(link))
 	else:
 		nlf_link_lable.config(text="Enter a valid search query to proceed!", fg='red', )
+		messagebox.showerror('Error', 'Enter a valid search query to proceed!')
 
 def close_window(count=8):
 	if count > 0:
@@ -41,41 +46,79 @@ def recSys_cb():
 		nlf_link_lable.config(text="Enter a valid search query first", fg='red', )
 
 def trending_today_cb():
-	pass
+	trn_lemmas="Suomi | Helsinki | "
+	trn_lbl=tk.Label(root, text=trn_lemmas )
+	trn_lbl.pack()
 
 # Create the main window
 root = tk.Tk()
 root.title("TAU | National Library of Finland Recommendation System")
-root.geometry('1200x450') 
+root.geometry('1000x550')
 
-label = tk.Label(root, text="Welcome\n\nWhat are you looking after, today?")
-label.pack(pady=25)
+icon_img = ImageTk.PhotoImage(Image.open("search-13-16.png").resize((10, 10),Image.Resampling.LANCZOS))
+root.iconphoto(True, icon_img)
+
+# Load left and right images from your local directory
+left_image_path = "tau.jpg"
+right_image_path = "nlf.png"
+
+left_image = Image.open(left_image_path)
+right_image = Image.open(right_image_path)
+
+left_image = left_image.resize((150, 150), Image.Resampling.LANCZOS)
+right_image = right_image.resize((150, 150), Image.Resampling.LANCZOS)
+
+tk_left_image = ImageTk.PhotoImage(left_image)
+tk_right_image = ImageTk.PhotoImage(right_image)
+
+# Set the images on labels
+left_image_label = tk.Label(root, image=tk_left_image)
+right_image_label = tk.Label(root, image=tk_right_image)
+
+# left_image_label.pack(side="top", anchor="w", pady=5)
+# right_image_label.pack(side="top", anchor="e", pady=5)
+
+
+# Position the images using grid
+left_image_label.grid(row=0, column=0, padx=5, pady=15)
+right_image_label.grid(row=0, column=1, padx=5, pady=15)
+
+
+# Add a label right after the images
+welcome_label = tk.Label(root, text="Welcome!\n\nWhat are you looking after, today?")
 
 # # Create and pack widgets Set default text with gray color
-entry = tk.Entry(root, width=55, fg='grey')
+entry = tk.Entry(root, width=55, fg='grey', borderwidth=5)
 entry.insert(0, "Query keywords...")
-entry.pack(pady=20)
 
 # Bind events to the Entry widget
 entry.bind('<FocusIn>', on_entry_click)
 entry.bind('<FocusOut>', on_entry_leave)
 
-search_button = tk.Button(root, text="Search NLF", padx=50, command=generate_link)
-search_button.pack(pady=2)
-
+search_btn = tk.Button(root, text="Search NLF", command=generate_link)
 nlf_link_lable = tk.Label(root, text="", fg='blue', cursor='arrow')
-nlf_link_lable.pack(pady=1)
 
+
+# trending_btn = tk.Button(root, text="Popular Today!", command=trending_today_cb)
+# trending_btn.pack()
+
+rec_btn = tk.Button(root, text="Recommend Me!", command=recSys_cb)
 recys_label = tk.Label(root, text="")
-recys_label.pack(pady=20)
 
-search_button = tk.Button(root, text="Recommend Me!", padx=75, command=recSys_cb)
-search_button.pack(pady=20)
 
 exit_btn = tk.Button(root, text="Exit", command=lambda: close_window())
-exit_btn.pack(pady=20)
-
 countdown_label = tk.Label(root, text="")
-countdown_label.pack(pady=10)
+
+welcome_label.grid(row=1, column=0, columnspan=2, pady=5)
+entry.grid(row=2, column=0, columnspan=2, pady=10)
+
+search_btn.grid(row=3, column=0, columnspan=2, pady=5)
+nlf_link_lable.grid(row=4, column=0, columnspan=2, pady=5)
+
+rec_btn.grid(row=5, column=0, columnspan=2, pady=5)
+recys_label.grid(row=6, column=0, columnspan=2, pady=5)
+
+exit_btn.grid(row=7, column=0, columnspan=2, pady=5)
+countdown_label.grid(row=8, column=0, columnspan=2, pady=10)
 
 root.mainloop()
--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -8,10 +8,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=188G
+#SBATCH --mem=256G
 #SBATCH --partition=gpu
 #SBATCH --time=01-00:00:00
-#SBATCH --array=256-560
+#SBATCH --array=367-560
 #SBATCH --gres=gpu:v100:1
 
 user="`whoami`"
