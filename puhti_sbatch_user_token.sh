da7777a1e9a846ace53cb4f29b6ee8539332d960
Farid Alijani <farid.alijani@gmail.com>
2023-11-23 18:37:58 +0200

    367-370 small partition 373g

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=375G
+#SBATCH --mem=373G
 #SBATCH --partition=small
 #SBATCH --time=01-00:00:00
 #SBATCH --array=367-370
