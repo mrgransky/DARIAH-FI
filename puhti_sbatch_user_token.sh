91fba45efa2028b6bd4613441ed859a15d1e6820
Farid Alijani <farid.alijani@gmail.com>
2023-11-23 18:49:20 +0200

    367-369 large partition 374g

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -8,10 +8,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=373G
-#SBATCH --partition=small
-#SBATCH --time=01-00:00:00
-#SBATCH --array=367-370
+#SBATCH --mem=374G
+#SBATCH --partition=large
+#SBATCH --time=00-23:59:00
+#SBATCH --array=367-369
 ####SBATCH --gres=gpu:v100:1
 
 user="`whoami`"
