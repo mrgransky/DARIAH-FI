7408d09d17761ac4211575593a3b84d2ab306d83
Farid Alijani <farid.alijani@gmail.com>
2023-11-24 16:21:51 +0200

    gpu 374g concat check

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=375G
+#SBATCH --mem=374G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
 #SBATCH --gres=gpu:v100:1
