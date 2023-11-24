36d7fa8c25c1b4f157bdeefb8207b4725376bfcb
Farid Alijani <farid.alijani@gmail.com>
2023-11-24 12:57:40 +0200

    hugemem concat 573g

--- gui.py
+++ gui.py
@@ -49,11 +49,14 @@ def trending_today_cb():
 	trn_lemmas="Suomi | Helsinki | "
 	trn_lbl=tk.Label(root, text=trn_lemmas )
 
-def clean_entry():
+def clean_search_entry():
 	entry.delete(0, "end")  # delete all the text in the entry
 	entry.insert(0, 'Query keywords...')
 	entry.config(fg='grey')
 	nlf_link_lable.destroy()
+
+def clean_recsys_entry():
+	return
 
 root = tk.Tk()
 root.title("TAU | National Library of Finland Recommendation System")
@@ -77,42 +80,44 @@ tk_right_image = ImageTk.PhotoImage(right_image)
 left_image_lbl = tk.Label(root, image=tk_left_image)
 right_image_lbl = tk.Label(root, image=tk_right_image)
 
-welcome_lbl = tk.Label(root, text="Welcome! What are you looking after, today?", font=('Georgia 15'))
+welcome_lbl = tk.Label(root, text="Welcome!\nWhat are you looking after, today?", font=('Georgia 13'), justify="center")
 
-entry = tk.Entry(root, width=30, fg='grey', borderwidth=5, font=('Georgia 12'))
+entry = tk.Entry(root, width=120, fg='grey', bg="purple", borderwidth=5, font=('Georgia 12'))
 entry.insert(0, "Query keywords...")
 
 entry.bind('<FocusIn>', on_entry_click)
 entry.bind('<FocusOut>', on_entry_leave)
 
-search_btn=tk.Button(root, text="Search NLF", command=generate_link)
+search_btn=tk.Button(root, text="Search NLF", width=20, command=generate_link)
+clean_search_btn = tk.Button(root, text="Clean", width=20, command=clean_search_entry)
 nlf_link_lable = tk.Label(root, text="", fg='blue', cursor='hand2')
-
-clean_btn = tk.Button(root, text="Clean", command=clean_entry)
 
 entry.bind('<FocusIn>', on_entry_click)
 entry.bind('<FocusOut>', on_entry_leave)
 
-rec_btn = tk.Button(root, text="Recommend Me!", command=recSys_cb)
+rec_btn = tk.Button(root, text="Recommend Me", width=20, command=recSys_cb)
+clean_recsys_btn = tk.Button(root, text="Clear", width=20, command=clean_recsys_entry)
 recys_lbl = tk.Label(root, text="")
 
-exit_btn = tk.Button(root, text="Exit", command=lambda: close_window())
+exit_btn = tk.Button(root, text="Exit", width=15, command=lambda: close_window())
 countdown_lbl = tk.Label(root, text="")
 
-left_image_lbl.grid(row=0, column=0, pady=20,)
-right_image_lbl.grid(row=0, column=3, pady=20,)
+left_image_lbl.grid(row=0, column=0, pady=20, padx=0,)
+right_image_lbl.grid(row=0, column=2, pady=20, padx=0,)
 
-welcome_lbl.grid(row=1, column=2, columnspan=4, pady=15, padx=0)
-entry.grid(row=2, column=0, columnspan=4, pady=15, padx=0)
+welcome_lbl.grid(row=1, column=1, padx=0, pady=0)
 
-# search_btn.grid(row=3, column=0, columnspan=4, pady=5)
-# clean_btn.grid(row=3, column=1, pady=5)
-# nlf_link_lable.grid(row=4, column=0, columnspan=4, pady=5, padx=0)
+entry.grid(row=2, column=0, columnspan=3, padx=0, pady=0, )
 
-# rec_btn.grid(row=5, column=0, columnspan=4, pady=5)
-# recys_lbl.grid(row=6, column=0, columnspan=4, pady=5)
+search_btn.grid(row=3, column=0, padx=0, pady=0)
+clean_search_btn.grid(row=3, column=2, padx=0, pady=0)
+nlf_link_lable.grid(row=4, column=0, padx=0, pady=0)
 
-# exit_btn.grid(row=7, column=0, columnspan=4, pady=5)
-# countdown_lbl.grid(row=8, column=0, columnspan=4, pady=10)
+rec_btn.grid(row=5, column=0, padx=0, pady=0)
+clean_recsys_btn.grid(row=5, column=2, padx=0, pady=0)
+recys_lbl.grid(row=6, column=1, padx=0, pady=0)
+
+exit_btn.grid(row=7, column=1, padx=0, pady=0)
+countdown_lbl.grid(row=8, column=1, padx=0, pady=0)
 
 root.mainloop()
