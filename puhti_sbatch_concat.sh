8d69f929d853187f8c45be4fd00dc4d01f371435
Farid Alijani <farid.alijani@gmail.com>
2023-11-24 13:10:40 +0200

    longrun concat 373g no gres

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,9 +8,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=373G
-#SBATCH --partition=small
-#SBATCH --time=03-00:00:00
+#SBATCH --mem=375G
+#SBATCH --partition=longrun
+#SBATCH --time=06-00:00:00
 #####SBATCH --gres=gpu:v100:1
 
 user="`whoami`"
