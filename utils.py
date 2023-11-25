89d1fce75a4cced336c4668309ed08396fea9568
Farid Alijani <farid.alijani@gmail.com>
2023-11-25 19:56:12 +0200

    elapsed time for each spm

--- utils.py
+++ utils.py
@@ -972,7 +972,7 @@ def get_idfed_users_norm(spMtx, idf_vec, save_dir: str="savin_dir", prefix_fname
 def get_user_token_spm_concat(SPMs, save_dir: str="savin_dir", prefix_fname: str="file_prefix"):
 	# SPMs: [(spm1, spm1_row, spm1_col), (spm1, spm1_row, spm1_col), ..., (spmN, spmN_row, spmN_col)]
 	# SPMs=[(load_pickle(fpath=spm_path), load_pickle(fpath=spm_uname_path), load_pickle(fpath=spm_tkname_path)) for spm_path, spm_uname_path, spm_tkname_path in zip( get_spm_files(fpath=spm_fname), get_spm_files(fpath=spm_rows_fname), get_spm_files(fpath=spm_cols_fname)) ]
-	print(f"<<>> Concatinating {len(SPMs)} SPMs, might take a while...")
+	print(f"Concatinating {len(SPMs)} SPMs".center(160, " "))
 	# return None, None, None
 	t=time.time()
 	ROWs=list()
@@ -998,7 +998,8 @@ def get_user_token_spm_concat(SPMs, save_dir: str="savin_dir", prefix_fname: str
 	current_matrix=lil_matrix((len(rownames_all), len(colnames_all)), dtype=np.float32)
 	for idx, val in enumerate(SPMs):
 		matrix, rownames, colnames=val
-		print(f"SPM {idx+1}/{len(SPMs)} {matrix.shape} {str(rownames[:3])} {str(colnames[:10])}")
+		t00=time.time()
+		print(f"SPM {idx+1}/{len(SPMs)} {matrix.shape} {str(rownames[:3])} {str(colnames[:10])}", end=" ")
 		# print(current_row_idx, current_col_idx)
 		if idx==len(SPMs)-1:
 			row_reverseindex_i=row_reverseindex[current_row_idx:]
@@ -1010,8 +1011,8 @@ def get_user_token_spm_concat(SPMs, save_dir: str="savin_dir", prefix_fname: str
 		newmatrix[np.ix_(row_reverseindex_i,col_reverseindex_i)]+=matrix
 		current_row_idx+=len(rownames)
 		current_col_idx+=len(colnames)
-		# print("#"*50)
-	print(f"Elapsed_t: {time.time()-t:.2f} sec".center(150, "-"))
+		print(f"elapsed_t: {time.time()-t00:.1f} s")
+	print(f"Total Contatenation Elapsed Time: {int(time.time()-t)} sec".center(160, " "))
 
 	spm_fname=os.path.join(save_dir, f"{prefix_fname}_USERs_TOKENs_spm_{newmatrix.shape[0]}_nUSRs_x_{newmatrix.shape[1]}_nTOKs.gz")
 	spm_rows_fname=os.path.join(save_dir, f"{prefix_fname}_USERs_TOKENs_spm_user_ip_names_{newmatrix.shape[0]}_nUSRs.gz")
