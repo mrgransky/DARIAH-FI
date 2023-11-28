f1d9900e7182d125674e4a1502ad202f7ac67e03
Farid Alijani <farid.alijani@gmail.com>
2023-11-29 00:15:24 +0200

    x182 hugemem_longrun 656g

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,9 +8,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=599G
-#SBATCH --partition=hugemem
-#SBATCH --time=02-23:00:00
+#SBATCH --mem=656G
+#SBATCH --partition=hugemem_longrun
+#SBATCH --time=04-23:00:00
 ####SBATCH --gres=gpu:v100:1
 
 user="`whoami`"
