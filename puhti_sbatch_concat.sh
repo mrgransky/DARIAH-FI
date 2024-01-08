26ed1646db9b615d156311515cbbbc141e0c2d3b
Farid Alijani <farid.alijani@gmail.com>
2024-01-08 21:20:38 +0200

    check mem 1500

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=1508G
+#SBATCH --mem=1500G
 #SBATCH --partition=hugemem_longrun
 #SBATCH --time=14-00:00:00
 ####SBATCH --gres=gpu:v100:1
