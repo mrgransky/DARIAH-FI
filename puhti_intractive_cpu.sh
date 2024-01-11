9181d1c0ac44fad852070dfa1a06941099888f02
Farid Alijani <farid.alijani@gmail.com>
2024-01-11 09:49:21 +0200

    large partition interactive debug

--- puhti_intractive_cpu.sh
+++ puhti_intractive_cpu.sh
@@ -7,7 +7,7 @@
 
 # Image_Retrieval_TUNI
 # srun -J intrtv_cpu --account=project_2004072 --partition=test --time=00-00:15:00 --mem=185G --ntasks=1 --cpus-per-task=16 --pty /bin/bash -i
-srun -J intrtv_cpu --account=project_2004072 --partition=small --time=00-03:00:00 --mem=128G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J intrtv_cpu --account=project_2004072 --partition=large --time=00-08:00:00 --mem=168G --ntasks=1 --cpus-per-task=4 --pty /bin/bash -i
 # srun -J cpu_large_interactive --account=project_2004072 --partition=large --time=00-23:59:00 --mem=64G --ntasks=1 --cpus-per-task=2 --pty /bin/bash -i
 
 module load git
