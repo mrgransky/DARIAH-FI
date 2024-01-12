600930614eadfb9ec1e25b68e65654ce6243b6ce
Farid Alijani <farid.alijani@gmail.com>
2024-01-12 10:04:13 +0200

    gpu 256g 16 cpus

--- puhti_intractive_gpu.sh
+++ puhti_intractive_gpu.sh
@@ -6,6 +6,6 @@
 # $ source puhti_intractive_gpu.sh
 
 # srun -J v100_gpu --account=project_2004072 --partition=gpu --gres=gpu:v100:1 --time=0-01:59:00 --mem=116G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
-srun -J v100_gputest --account=project_2004072 --partition=gputest --gres=gpu:v100:1 --time=0-00:15:00 --mem=256G --ntasks=1 --cpus-per-task=10 --pty /bin/bash -i
+srun -J gpu_interactive --account=project_2004072 --partition=gpu --gres=gpu:v100:1 --time=0-00:15:00 --mem=256G --ntasks=1 --cpus-per-task=16 --pty /bin/bash -i
 
 module load git
