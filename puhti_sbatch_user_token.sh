cae307fec6229bca6222cf3faee05902be1d10fc
Farid Alijani <farid.alijani@gmail.com>
2024-01-17 21:09:05 +0200

    save token_data_df to speedup weight learning sbatch

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=150G
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=180G
 #SBATCH --partition=large
 #SBATCH --time=03-00:00:00
 #SBATCH --array=712-729
