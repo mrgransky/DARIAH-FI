6327f0f1ea92515e9a4f8c614646bc7c54fb37c1
Farid Alijani <farid.alijani@gmail.com>
2024-01-31 13:18:58 +0200

    user_token nikeXY test x2 cpus

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --mem=199G
 #SBATCH --partition=large
 #SBATCH --time=03-00:00:00
