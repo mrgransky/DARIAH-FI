8b4b35552a2edda5d3738e57372c507053ff771e
Farid Alijani <farid.alijani@gmail.com>
2024-01-31 18:53:52 +0200

    spm_xx58 concat small partition 1cpu

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -7,10 +7,10 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=373G
-#SBATCH --partition=large
-#SBATCH --time=03-00:00:00
+#SBATCH --partition=small
+#SBATCH --time=02-00:00:00
 ####SBATCH --gres=gpu:v100:1
 
 user="`whoami`"
