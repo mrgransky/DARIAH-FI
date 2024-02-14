8af88687c1bcc47113db81cf7e17603f2b87e2f7
Farid Alijani <farid.alijani@gmail.com>
2024-02-14 10:50:46 +0200

    0 140 large partition

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -7,12 +7,12 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=177G
-#SBATCH --partition=gpu
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=373G
+#SBATCH --partition=large
 #SBATCH --time=03-00:00:00
 #SBATCH --array=0-140
-#SBATCH --gres=gpu:v100:1
+####SBATCH --gres=gpu:v100:1
 
 ##### x302: 0-150, 580-731
 
