38b92f9b268f7300bd7bfaebccbfc8829a67f01e
Farid Alijani <farid.alijani@gmail.com>
2024-03-01 13:37:43 +0200

    xy 256g 1 cpu

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=299G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=256G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
 #SBATCH --array=730-731
