12d8be08e39b71d473dece6571d3b9ba088c20a9
Farid Alijani <farid.alijani@gmail.com>
2024-03-06 18:15:18 +0200

    683-724 pouta check parallel nohup

--- pouta_preprocess_docs.sh
+++ pouta_preprocess_docs.sh
@@ -5,7 +5,7 @@
 ## $ nohup bash pouta_preprocess_docs.sh 0 > /media/volume/trash/NLF/nlf_preprocess_parallel_jobs.out 2>&1 & # with output saved in check_output.out
 
 ## $ nohup parallel -j 50 "bash pouta_preprocess_docs.sh {}" ::: {682..731} > /dev/null 2>&1 &
-## $ nohup parallel -j 50 "bash pouta_preprocess_docs.sh {}" ::: {683..731} > /media/volume/trash/NLF/nlf_preprocess_docs_parallel_jobs.out 2>&1 &
+## $ nohup parallel -j 50 "bash pouta_preprocess_docs.sh {}" ::: {683..724} > /media/volume/trash/NLF/nlf_preprocess_docs_parallel_jobs.out 2>&1 &
 
 USR_NAME="`whoami`"
 stars=$(printf '%*s' 100 '')
