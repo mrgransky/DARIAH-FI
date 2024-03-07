f4f8ae381162a381284218575b8ac0e970b2f391
Farid Alijani <farid.alijani@gmail.com>
2024-03-07 19:44:11 +0200

    check for preprocess of 0-731 name nk_q with detailed print

--- utils.py
+++ utils.py
@@ -1426,11 +1426,9 @@ def get_preprocessed_document(dframe, preprocessed_docs_fpath):
 		print(f"Cleaning {len(raw_docs_list)} unique Raw Docs [Query Search + Collection + Clipping + Snippets + Content OCR]...")
 
 		pst = time.time()
-		with HiddenPrints(): # with no prints
-			preprocessed_docs = [cdocs for _, vsnt in enumerate(raw_docs_list) if ((cdocs:=clean_(docs=vsnt)) and len(cdocs)>1) ]
-		
-		# preprocessed_docs = [cdocs for _, vsnt in enumerate(raw_docs_list) if ((cdocs:=clean_(docs=vsnt)) and len(cdocs)>1) ]
-		
+		# with HiddenPrints(): # with no prints
+		# 	preprocessed_docs = [cdocs for _, vsnt in enumerate(raw_docs_list) if ((cdocs:=clean_(docs=vsnt)) and len(cdocs)>1) ]		
+		preprocessed_docs = [cdocs for _, vsnt in enumerate(raw_docs_list) if ((cdocs:=clean_(docs=vsnt)) and len(cdocs)>1) ]
 		print(f"Corpus of {len(preprocessed_docs)} raw docs [d1, d2, d3, ..., dN] created in {time.time()-pst:.1f} s")
 		save_pickle(pkl=preprocessed_docs, fname=preprocessed_docs_fpath)
 
