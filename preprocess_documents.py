08b688945106a6ff2434b13c035e4ed89eee1589
Farid Alijani <farid.alijani@gmail.com>
2024-03-15 00:34:30 +0200

    total elapsed time add for all

--- preprocess_documents.py
+++ preprocess_documents.py
@@ -68,6 +68,15 @@ def main():
 	)
 
 if __name__ == '__main__':
-	print(f"Started: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}".center(140, " "))
+	print(
+		f"Started: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+		.center(160, " ")
+	)
+	START_EXECUTION_TIME = time.time()
 	main()
-	print(f"Finished: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}".center(140, " "))
+	END_EXECUTION_TIME = time.time()
+	print(
+		f"Finished: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} "
+		f"TOTAL_ELAPSED_TIME: {END_EXECUTION_TIME-START_EXECUTION_TIME:.1f} sec"
+		.center(160, " ")
+	)
