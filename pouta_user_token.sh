4875fcd3c488a7df28e1d48c98ff46c9507fb65d
Farid Alijani <farid.alijani@gmail.com>
2024-03-14 16:28:13 +0200

    at least 4 chars cuda 3 715 718 pouta

--- pouta_user_token.sh
+++ pouta_user_token.sh
@@ -5,7 +5,7 @@
 ## $ nohup bash pouta_user_token.sh 0 > /media/volume/trash/NLF/check_nlf_output.out 2>&1 & # with output saved in check_output.out
 
 ## $ nohup parallel -j 8 "bash pouta_user_token.sh {}" ::: {682..731} > /dev/null 2>&1 &
-## $ nohup parallel -j 4 "bash pouta_user_token.sh {}" ::: {729..731} > /media/volume/trash/NLF/nlf_usr_tk_parallel_jobs_cuda1_729_731.out 2>&1 &
+## $ nohup parallel -j 4 "bash pouta_user_token.sh {}" ::: {715..718} > /media/volume/trash/NLF/nlf_usr_tk_parallel_jobs_cuda3_715_718.out 2>&1 &
 
 USR_NAME="`whoami`"
 stars=$(printf '%*s' 100 '')
@@ -34,7 +34,7 @@ echo "${stars// /*}"
 if [ $qIDX -ge 0 ] && [ $qIDX -lt ${#files[@]} ]; then
 echo "Processing Q[$qIDX]: ${files[$qIDX]}"
 	python -u user_token.py \
-		--cudaNum 2 \
+		--cudaNum 3 \
 		--inputDF ${files[$qIDX]} \
 		--outDIR $DATAFRAME_DIR \
 		--maxNumFeat $maxNumFeatures >>$STORAGE_DIR/Nationalbiblioteket/trash/nk_q_$qIDX.out 2>&1 &
