53b8a3b9d384273727393d24bd087a45d1915303
Farid Alijani <farid.alijani@gmail.com>
2024-03-18 16:26:48 +0200

    x0-80,726-731 all together 308g

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=256G
+#SBATCH --mem=308G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
 #SBATCH --array=726-731,0-80
