5a30947911c256bdc6a402a501e076991f27c770
Farid Alijani <farid.alijani@gmail.com>
2024-03-19 23:57:40 +0200

    x93 concat check puhti 420g hugemem

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,8 +8,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=371G
-#SBATCH --partition=small
+#SBATCH --mem=420G
+#SBATCH --partition=hugemem
 #SBATCH --time=03-00:00:00
 ####SBATCH --gres=gpu:v100:1
 
