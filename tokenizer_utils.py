5645ea3977bd1ba9f20fdce3bfdee6cc5da2f7a1
Farid Alijani <farid.alijani@gmail.com>
2024-03-23 20:29:50 +0200

    at least 5 characters for vocab

--- tokenizer_utils.py
+++ tokenizer_utils.py
@@ -102,12 +102,12 @@ def stanza_lemmatizer(docs: str="This is a <NORMAL> document!", device=None):
 			re.sub(r'[";&#<>_\-\+\^\.\$\[\]]', '', wlm.lower())
 			for _, vsnt in enumerate(all_.sentences) 
 			for _, vw in enumerate(vsnt.words) 
-			if ( 
-					(wlm:=vw.lemma)
-					and 4 <= len(wlm) <= 40
-					and not re.search(r'\b(?:\w*(\w)(\1{2,})\w*)\b|<ros>|<eos>|<EOS>|<sos>|<SOS>|<UNK>|<unk>|\^|\s+', wlm) 
-					and vw.upos not in useless_upos_tags 
-					and wlm not in UNQ_STW
+			if (
+				(wlm:=vw.lemma)
+				and 5 <= len(wlm) <= 43
+				and not re.search(r'\b(?:\w*(\w)(\1{2,})\w*)\b|<ros>|<eos>|<EOS>|<sos>|<SOS>|<UNK>|<unk>|\^|\s+', wlm) 
+				and vw.upos not in useless_upos_tags 
+				and wlm not in UNQ_STW
 			)
 		]
 		end_t = time.time()
