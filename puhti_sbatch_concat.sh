9dc473b63e3461a9cc668cb589994e53acfc1247
Farid Alijani <farid.alijani@gmail.com>
2024-04-12 10:52:56 +0300

    x377 hugemem 473g

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -7,9 +7,9 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=373G
-#SBATCH --partition=large
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=473G
+#SBATCH --partition=hugemem
 #SBATCH --time=03-00:00:00
 ####SBATCH --gres=gpu:v100:1
 
