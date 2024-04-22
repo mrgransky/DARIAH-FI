af956aed816b1657bb793ffa17cd58281aa5bbf4
Farid Alijani <farid.alijani@gmail.com>
2024-04-22 22:25:08 +0300

    user token cleaned version

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=300G
+#SBATCH --mem=370G
 #SBATCH --partition=small
 #SBATCH --time=03-00:00:00
 ####SBATCH --gres=gpu:v100:1
