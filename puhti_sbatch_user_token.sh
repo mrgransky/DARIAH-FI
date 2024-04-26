8169dc73502a8eaa8bf0ab87f6b70dce154e5f01
Farid Alijani <farid.alijani@gmail.com>
2024-04-26 21:45:52 +0300

    x732 for all run over weekend

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -11,7 +11,7 @@
 #SBATCH --mem=108G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
-#SBATCH --array=0-364,697-731
+#SBATCH --array=364-700
 #SBATCH --gres=gpu:v100:1
 
 #### SBATCH --array=39,48-49
