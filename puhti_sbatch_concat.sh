1598f70fb02da03cef44d8d55815b92513cf5380
Farid Alijani <farid.alijani@gmail.com>
2024-05-15 21:29:48 +0300

    concat x58 large 356g 1day

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,9 +8,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=372G
+#SBATCH --mem=256G
 #SBATCH --partition=large
-#SBATCH --time=03-00:00:00
+#SBATCH --time=01-00:00:00
 ####SBATCH --gres=gpu:v100:1
 
 user="`whoami`"
