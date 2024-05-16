90c2e67caabe07e95685104265b700fd0add46e6
Farid Alijani <farid.alijani@gmail.com>
2024-05-17 00:29:05 +0300

    400-731 final cleanedup

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -8,10 +8,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=100G
+#SBATCH --mem=79G
 #SBATCH --partition=large
 #SBATCH --time=03-00:00:00
-#SBATCH --array=0-399
+#SBATCH --array=400-731
 ####SBATCH --gres=gpu:v100:1
 
 #### SBATCH --array=39,48-49
