10d3ac61745a6701b19cb64e18f1257e08f30b47
Farid Alijani <farid.alijani@gmail.com>
2024-05-16 17:35:19 +0300

    365-727

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -11,7 +11,7 @@
 #SBATCH --mem=110G
 #SBATCH --partition=large
 #SBATCH --time=03-00:00:00
-#SBATCH --array=0-364
+#SBATCH --array=365-727
 ####SBATCH --gres=gpu:v100:1
 
 #### SBATCH --array=39,48-49
