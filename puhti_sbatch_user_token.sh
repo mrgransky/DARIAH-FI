e787d0ef86d925c47079013eac3d1c95a9197a0d
Farid Alijani <farid.alijani@gmail.com>
2024-05-30 17:42:26 +0300

    more meaningless lemmas to be excluded SPM again

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -8,9 +8,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=80G
+#SBATCH --mem=70G
 #SBATCH --partition=small
-#SBATCH --time=01-00:00:00
+#SBATCH --time=00-04:00:00
 #SBATCH --array=0-399
 ####SBATCH --gres=gpu:v100:1
 
