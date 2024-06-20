117be5a2630c2c8fbb491207d78e2db53b73426d
Farid Alijani <farid.alijani@gmail.com>
2024-06-20 21:34:58 +0300

    additional sparse stanza 400-731 small

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -8,11 +8,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=109G
-#SBATCH --partition=gpu
+#SBATCH --mem=98G
+#SBATCH --partition=small
 #SBATCH --time=00-01:00:00
-#SBATCH --array=0-399
-#SBATCH --gres=gpu:v100:1
+#SBATCH --array=400-731
+###SBATCH --gres=gpu:v100:1
 
 #### SBATCH --array=39,48-49
 ##### x303: 0-150,580-732
