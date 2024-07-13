0d8aaa7ed90295e01e731fab1d1ca8c5bb3a6635
Farid Alijani <farid.alijani@gmail.com>
2024-07-13 20:14:02 +0300

    0-399 user token trankit large

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=2
 #SBATCH --mem=70G
 #SBATCH --partition=large
 #SBATCH --time=00-01:00:00
