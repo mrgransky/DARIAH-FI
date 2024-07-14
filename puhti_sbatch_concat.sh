ae5ec458bdab60994f1b3267d065f7f0fa8bb5b9
Farid Alijani <farid.alijani@gmail.com>
2024-07-14 18:45:18 +0300

    concat trankit

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --account=project_2004072
-#SBATCH --job-name=spMtx_x732_concat_stanza
+#SBATCH --job-name=spMtx_x732_concat_trankit
 #SBATCH --output=/scratch/project_2004072/Nationalbiblioteket/trash/NLF_logs/%x_%N_%j.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
@@ -30,8 +30,8 @@ echo "THREADS/CORE: $SLURM_THREADS_PER_CORE"
 echo "${stars// /*}"
 echo "<> Using $SLURM_CLUSTER_NAME conda env from tykky module..."
 dfsDIR="/scratch/project_2004072/Nationalbiblioteket/dataframes_x732" ########## must be adjusted! ##########
-LEMMATIZER="stanza"
-# LEMMATIZER="trankit"
+# LEMMATIZER="stanza"
+LEMMATIZER="trankit"
 
 # for qu in 'Suomen kansallismuseo' 'Uudenkaupungin työväentalo' 'naiset rauha demokratia ihmisoikeudet' 'Lastensuojelupalvelut' 'Eläinten oikeudet' 'Sosiologisen analyysin harjoitus' 'naisten yhteiskunnallista roolia' 'olympiakomitea' 'Iltapäivätoiminta' 'Vapaaehtoistoimintaan osallistuminen suomalaisessa yhteiskunnassa' 'koulutusjärjestelmien kehittäminen Suomessa' 'Ilmastonmuutos Suomen luonnossa' 'Ulkomaisten luonnonvarojen käyttö' 'seminaarin opetuksen kehittäminen' 'Ihmisten ja muiden eläinten oikeuksista' 'Vähemmistöt' 'Suomen Maanviljelys Teollisuus-Pankki Osakeyhtiö' 'helsingin sanomat' 'Lahden Lehti' 'liikepankki osakeyhtiö' 'TAMPEREEN TEHDAS' 'Sanomalehti Länsi-Suomi' 'pohjois-hämeen osuuspankki' 'Helsingin Teknillinen reaalikoulu' 'egentlig och centraliserad socialförvaltning' 'ensamkommande barn och flyktingar' 'psykisk funktionsnedsättning' 'likvärdig och rättssäker socialtjänst och funktionshinderomsorg' 'Suomen Teknillinen Korkeakoulu' 'Juha Sipilä Sahalahti' 'Liberalismin ja konservatismin aika' 'Suomalaisten suhtautuminen konservatismiin' 'Helsingin poliisilaitos' 'Suomen sosialistinen tasavalta' 'Kansalliskirjasto' 'HELSINGIN UUSI YHTEISKOULU' 'Mietteitä sosialismista' 'suomen sosialidemokraattinen puolue' 'Kokoomuksen historia' 'Suomen Kommunistinen Puolue' 'Suomen Teollisuuslehti' 'stalinismi ja länsimaat' 'antisemitismi' 'sotilaiden päihteiden käyttö toisen maailmansodan aikana' 'Marxismi-leninismi' 'Sosiaalisen kestävyyden' 'sosiaalinen oikeudenmukaisuus' 'Helsingfors Gymnastikklubb' 'suomen kestävän kehityksen tavoitteet' 'Kirkosta eroaminen Suomessa' 'suomalaisten uskonnollisuus' 'Suomen ortodoksiseen kirkkoon' 'Finlands Socialdemokratiska Parti' 'Tampereen seudun työväenopisto' 'Helsingin tuomiokirkko' 'sosialismi' 'Helsingin pörssi ja suomen pankki' 'suomen pääkaupunki' 'Länsi-Uudenmaan poliisilaitos' 'tampereen teknillinen yliopisto' 'torvisoittokunta' 'Tampereen Työväen Teatteri' 'Suomen pankki lainat ja talletukset' 'Global Warming' 'Economical Crisis in Finland' 'Helsingin Kaupunginteatteri' 'Suomalainen Kirjakauppa' 'kantakirjasonni' 'Senaatti-kiinteistöt ja Helsingin kaupunki' 'finska skolor på åland' 'Helsingfors stadsteater' 'Åbo Akademi i Vasa' 'Stockholms universitet' 'Jakobstads svenska församling' 'Ålands kulturhistoriska museum'
 for qu in 'naiset rauha demokratia ihmisoikeudet'
