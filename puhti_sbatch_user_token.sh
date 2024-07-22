dc7e7d6800f956bb3acd2b91ee7a56663c49c78c
Farid Alijani <farid.alijani@gmail.com>
2024-07-22 19:59:05 +0300

    90g gpu 400-731

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -8,10 +8,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=70G
+#SBATCH --mem=90G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
-#SBATCH --array=0-399
+#SBATCH --array=400-731
 #SBATCH --gres=gpu:v100:1
 
 user="`whoami`"
