c6f1d8b54ec155d041b81522073f7440855a7c05
Farid Alijani <farid.alijani@gmail.com>
2024-07-30 12:52:01 +0300

    additional sparse 0-399 gpu

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #SBATCH --account=project_2004072
-#SBATCH --job-name=nikeQ_stanza
+#SBATCH --job-name=nikeQ_stanza_additional_sparse
 #SBATCH --output=/scratch/project_2004072/Nationalbiblioteket/trash/NLF_logs/%x_%a_%N_%j_%A.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=90G
+#SBATCH --mem=95G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
-#SBATCH --array=400-731
+#SBATCH --array=0-399
 #SBATCH --gres=gpu:v100:1
 
 user="`whoami`"
