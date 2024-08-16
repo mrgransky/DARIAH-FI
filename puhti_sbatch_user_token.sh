d02e4f026462fa4e02d7a0203bf4b9996019738f
Farid Alijani <farid.alijani@gmail.com>
2024-08-16 21:11:25 +0300

    small 91g 0-399 update meaningless more

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -8,11 +8,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=91G
-#SBATCH --partition=gpu
+#SBATCH --mem=90G
+#SBATCH --partition=small
 #SBATCH --time=00-01:00:00
 #SBATCH --array=0-399
-#SBATCH --gres=gpu:v100:1
+###SBATCH --gres=gpu:v100:1
 
 user="`whoami`"
 stars=$(printf '%*s' 100 '')
