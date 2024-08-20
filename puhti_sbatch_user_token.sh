b5490052b97f02e9f6348e2b2fe59cf8b2cf1fff
Farid Alijani <farid.alijani@gmail.com>
2024-08-20 13:36:03 +0300

    major cleaning of meaningless lemmas small 0-399

--- puhti_sbatch_user_token.sh
+++ puhti_sbatch_user_token.sh
@@ -9,9 +9,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=90G
-#SBATCH --partition=gpu
+#SBATCH --partition=small
 #SBATCH --time=00-01:00:00
-#SBATCH --array=400-732
+#SBATCH --array=0-399
 #SBATCH --gres=gpu:v100:1
 
 user="`whoami`"
