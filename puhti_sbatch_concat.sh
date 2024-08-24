8bd98bbd090687f58b3211731a6d730217917275
Farid Alijani <farid.alijani@gmail.com>
2024-08-24 21:07:42 +0300

    0-399 small additional sparse more suffixes add

--- puhti_sbatch_concat.sh
+++ puhti_sbatch_concat.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=480G
+#SBATCH --mem=470G
 #SBATCH --partition=hugemem
 #SBATCH --time=03-00:00:00
 ####SBATCH --gres=gpu:v100:1
